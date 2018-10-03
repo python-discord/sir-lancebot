b0fa8b78f107647315d61ecaae024793cd8b33cd
Marko Kovačević <markokovacevic@email.cz>
2018-10-03 19:44:05 +0200

    Use docstrings instead of block comments.

--- bot/cogs/template.py
+++ bot/cogs/template.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 class Template:
 
     """
-        A template cog that contains examples of commands and command groups.
+    A template cog that contains examples of commands and command groups.
     """
 
     def __init__(self, bot):
@@ -16,12 +16,18 @@ class Template:
 
     @commands.group(name='git', invoke_without_command=True)
     async def github(self, ctx):
-        # A command group with the name git. You can now create sub-commands such as git commit.
+        """
+        A command group with the name git. You can now create sub-commands such as git commit.
+        """
+
         await ctx.send('Resources to learn **Git**: https://try.github.io/.')
 
     @github.command()
     async def commit(self, ctx):
-        # A command that belongs to the git command group. Invoked using git commit.
+        """
+        A command that belongs to the git command group. Invoked using git commit.
+        """
+
         await ctx.send('`git commit -m "First commit"` commits tracked changes.')
 
 
