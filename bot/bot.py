3df977363ec411b9fc81081ecb41f61d209c8839
Neko (Is Lewd) <nekoislewd@gmail.com>
2018-10-07 14:32:19 +0100

    quick fix
    Wrong import order, don't `!` as the prefix as it is overused.

--- bot/bot.py
+++ bot/bot.py
@@ -1,13 +1,13 @@
+from os import environ
 from pathlib import Path
 from sys import stderr
 from traceback import print_exc
-from os import environ
 
 from discord.ext import commands
 
 
 HACKTOBERBOT_TOKEN = environ.get('HACKTOBERBOT_TOKEN')
-bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'))
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('h!'))
 
 if __name__ == '__main__':
     # Scan for files in the /cogs/ directory and make a list of the file names.
