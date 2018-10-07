8ba3e0a45ce9f460a421c100004da8b17ec0832c
sco1 <sco1.git@gmail.com>
2018-10-07 17:44:05 -0400

    Update prefixes
    Spoooooky

--- bot/bot.py
+++ bot/bot.py
@@ -1,13 +1,12 @@
+from os import environ
 from pathlib import Path
 from sys import stderr
 from traceback import print_exc
-from os import environ
 
 from discord.ext import commands
 
-
 HACKTOBERBOT_TOKEN = environ.get('HACKTOBERBOT_TOKEN')
-bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'))
+bot = commands.Bot(command_prefix=commands.when_mentioned_or(".", "👻 ", "👻"))
 
 if __name__ == '__main__':
     # Scan for files in the /cogs/ directory and make a list of the file names.
