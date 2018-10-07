bee600fa2a94bdf0f4fc826c0914d012f28e9988
sco1 <sco1@users.noreply.github.com>
2018-10-07 15:26:06 -0700

    Add Hacktoberfest user stats cog (#7)
    * Initialize Hacktoberfest stats cog
    
    * Fix generated grammar, add temporary variables to shorten string gen
    
    * Code review modifications
    
    Adjust variable & function names to be more in line with PEP8's
    recommendations.
    
    Break API query URL into multiple parts to aid readability and
    customization
    
    Adjust command input to explicitly accept a username input rather than
    star packing & unpacking. d.py guards against too many inputs for us
    
    Change _getURL to _get_shortname and adjust documentation because we're
    not even doing what the function name and documentation said we were
    
    * Remove hardcoded response channel
    
    * Update prefixes
    
    Spoooooky
    
    * Swap emoji for unicode string
    
    👻👻👻
    
    * Change unicode ghost string to a friendlier ghost string
    
    See: Casper, the Friendly Ghost

--- bot/bot.py
+++ bot/bot.py
@@ -1,13 +1,14 @@
+from os import environ
 from pathlib import Path
 from sys import stderr
 from traceback import print_exc
-from os import environ
 
 from discord.ext import commands
 
-
 HACKTOBERBOT_TOKEN = environ.get('HACKTOBERBOT_TOKEN')
-bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'))
+
+ghost_unicode = "\N{GHOST}"
+bot = commands.Bot(command_prefix=commands.when_mentioned_or(".", f"{ghost_unicode} ", ghost_unicode))
 
 if __name__ == '__main__':
     # Scan for files in the /cogs/ directory and make a list of the file names.
