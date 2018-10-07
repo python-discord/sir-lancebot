4a6599b082dac777798005e8e8ceb5ad3398700e
sco1 <sco1.git@gmail.com>
2018-10-07 18:01:36 -0400

    Swap emoji for unicode string
    👻👻👻

--- bot/bot.py
+++ bot/bot.py
@@ -6,7 +6,9 @@ from traceback import print_exc
 from discord.ext import commands
 
 HACKTOBERBOT_TOKEN = environ.get('HACKTOBERBOT_TOKEN')
-bot = commands.Bot(command_prefix=commands.when_mentioned_or(".", "👻 ", "👻"))
+
+ghost_unicode = u"\U0001F47B"
+bot = commands.Bot(command_prefix=commands.when_mentioned_or(".", f"{ghost_unicode} ", ghost_unicode))
 
 if __name__ == '__main__':
     # Scan for files in the /cogs/ directory and make a list of the file names.
