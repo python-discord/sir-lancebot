1ce65cf745d409550f36fa774825866c98d36cfe
hundredrab <sourab.jha@kgec.edu.in>
2018-10-11 01:40:26 +0530

    Fix #27: Add spooky reactions.

---
+++ bot/cogs/spookyreact.py
@@ -0,0 +1,32 @@
+from discord.ext import commands
+
+SPOOKY_TRIGGERS = {
+    'spooky': "\U0001F47B",
+    'skeleton': "\U0001F480",
+    'doot': "\U0001F480",
+    'pumpkin': "\U0001F383",
+    'halloween': "\U0001F383",
+    'jack-o-lantern': "\U0001F383"
+}
+
+
+class SpookyReact:
+
+    """
+    A cog that makes the bot react to message triggers.
+    """
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def on_message(self, ctx):
+        """
+        A command to send the hacktoberbot github project
+        """
+        for trigger in SPOOKY_TRIGGERS.keys():
+            if trigger in ctx.content.lower():
+                await ctx.add_reaction(SPOOKY_TRIGGERS[trigger])
+
+
+def setup(bot):
+    bot.add_cog(SpookyReact(bot))
