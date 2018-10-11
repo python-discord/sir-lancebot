f74bd643cfc5e6867c449a4f7efee501127b62c9
meooow25 <soumiksarkar.3120@gmail.com>
2018-10-11 10:42:57 +0530

    Relocate halloween facts to a JSON file

--- bot/cogs/halloween_facts.py
+++ bot/cogs/halloween_facts.py
@@ -1,42 +1,13 @@
 import asyncio
+import json
 import random
 from datetime import timedelta
+from pathlib import Path
 
 import discord
 from discord.ext import commands
 
-HALLOWEEN_FACTS = [
-    "Halloween or Hallowe'en is also known as Allhalloween, All Hallows' Eve and  All Saints' Eve.",
-    ("It is widely believed that many Halloween traditions originated from ancient Celtic harvest festivals, "
-     "particularly the Gaelic festival Samhain, which means \"summer's end\"."),
-    ("It is believed that the custom of making jack-o'-lanterns at Halloween began in Ireland. In the 19th century, "
-     "turnips or mangel wurzels, hollowed out to act as lanterns and often carved with grotesque faces, were used at "
-     "Halloween in parts of Ireland and the Scottish Highlands."),
-    "Halloween is the second highest grossing commercial holiday after Christmas.",
-    ("The word \"witch\" comes from the Old English *wicce*, meaning \"wise woman\". In fact, *wiccan* were highly "
-     "respected people at one time. According to popular belief, witches held one of their two main meetings, or "
-     "*sabbats*, on Halloween night."),
-    "Samhainophobia is the fear of Halloween.",
-    ("The owl is a popular Halloween image. In Medieval Europe, owls were thought to be witches, and to hear an owl's "
-     "call meant someone was about to die."),
-    ("An Irish legend about jack-o'-lanterns goes as follows:\n"
-     "*On route home after a night's drinking, Jack encounters the Devil and tricks him into climbing a tree. A "
-     "quick-thinking Jack etches the sign of the cross into the bark, thus trapping the Devil. Jack strikes a bargain "
-     "that Satan can never claim his soul. After a life of sin, drink, and mendacity, Jack is refused entry to heaven "
-     "when he dies. Keeping his promise, the Devil refuses to let Jack into hell and throws a live coal straight from "
-     "the fires of hell at him. It was a cold night, so Jack places the coal in a hollowed out turnip to stop it from "
-     "going out, since which time Jack and his lantern have been roaming looking for a place to rest.*"),
-    ("Trick-or-treating evolved from the ancient Celtic tradition of putting out treats and food to placate spirits "
-     "who roamed the streets at Samhain, a sacred festival that marked the end of the Celtic calendar year."),
-    ("Comedian John Evans once quipped: \"What do you get if you divide the circumference of a jack-o’-lantern by its "
-     "diameter? Pumpkin π.\""),
-    ("Dressing up as ghouls and other spooks originated from the ancient Celtic tradition of townspeople disguising "
-     "themselves as demons and spirits. The Celts believed that disguising themselves this way would allow them to "
-     "escape the notice of the real spirits wandering the streets during Samhain."),
-    ("In Western history, black cats have typically been looked upon as a symbol of evil omens, specifically being "
-     "suspected of being the familiars of witches, or actually shape-shifting witches themselves. They are, however, "
-     "too cute to be evil."),
-]
+HALLOWEEN_FACTS_JSON = "halloween_facts.json"
 SPOOKY_EMOJIS = [
     "\N{BAT}",
     "\N{DERELICT HOUSE BUILDING}",
@@ -56,6 +27,8 @@ class HalloweenFacts:
 
     def __init__(self, bot):
         self.bot = bot
+        with open(Path("resources", HALLOWEEN_FACTS_JSON), "r") as file:
+            self.halloween_facts = json.load(file)
         self.channel = None
         self.last_fact = None
 
@@ -68,7 +41,7 @@ class HalloweenFacts:
         A background task that runs forever, sending Halloween facts at random to the Discord channel with id equal to
         HACKTOBERFEST_CHANNEL_ID every INTERVAL seconds.
         """
-        facts = list(enumerate(HALLOWEEN_FACTS))
+        facts = list(enumerate(self.halloween_facts))
         while True:
             # Avoid choosing each fact at random to reduce chances of facts being reposted soon.
             random.shuffle(facts)
