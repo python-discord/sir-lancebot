216fea68051f682dee5ca82857cdda487bd98387
sco1 <sco1.git@gmail.com>
2018-10-13 15:29:30 -0400

    Add bot self-reaction short circuit, add logging

--- bot/cogs/spookyreact.py
+++ bot/cogs/spookyreact.py
@@ -1,4 +1,7 @@
+import logging
 import re
+
+import discord
 
 SPOOKY_TRIGGERS = {
     'spooky': (r"\bspo{2,}ky\b", "\U0001F47B"),
@@ -20,12 +23,18 @@ class SpookyReact:
     def __init__(self, bot):
         self.bot = bot
 
-    async def on_message(self, ctx):
+    async def on_message(self, ctx: discord.Message):
         """
         A command to send the hacktoberbot github project
 
         Lines that begin with the bot's command prefix are ignored
         """
+        await ctx.channel.send("spooky")
+        # Short-circuit if bot tries to react to itself
+        if ctx.author == self.bot.user:
+            logging.info(f"Bot self-react short-circuited. Message ID: {ctx.id}")
+            return
+
         # Check for & ignore messages with bot commands, since we're in on_message
         # we don't have a Context object, so we need to generate a temporary one
         tmp_ctx = await self.bot.get_context(ctx)
@@ -34,6 +43,9 @@ class SpookyReact:
                 trigger_test = re.search(SPOOKY_TRIGGERS[trigger][0], ctx.content.lower())
                 if trigger_test:
                     await ctx.add_reaction(SPOOKY_TRIGGERS[trigger][1])
+                    logging.info(f"Added '{trigger}' reaction to message ID: {ctx.id}")
+        else:
+            logging.info(f"Ignoring reaction on command invocation. Message ID: {ctx.id}")
 
 
 def setup(bot):
