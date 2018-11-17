f4c659b5260c9d17b69104047d25adeef1840ca4
Leon Sandøy <leon.sandoy@conmodo.com>
2018-11-18 00:34:44 +0100

    Azure Pipeline CI

---
+++ scripts/deploy-azure.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd ..
+
+# Build and deploy on master branch, only if not a pull request
+if [[ ($BUILD_SOURCEBRANCHNAME == 'master') && ($SYSTEM_PULLREQUEST_PULLREQUESTID == '') ]]; then
+    echo "Building image"
+    docker build -t pythondiscord/seasonalbot:latest -f docker/Dockerfile .
+
+    echo "Pushing image to Docker Hub"
+    docker push pythondiscord/seasonalbot:latest
+
+    echo "Deploying on server"
+    pepper $1 state.apply docker/hacktoberbot --out=no_out --non-interactive &> /dev/null
+else
+    echo "Skipping deploy"
+fi
