89c27d7e4c3c31cc62a94733293ae56e80516d4b
sco1 <sco1.git@gmail.com>
2019-03-04 12:10:01 -0500

    Add Cog inheritance to season loader

--- bot/seasons/season.py
+++ bot/seasons/season.py
@@ -345,7 +345,7 @@ class SeasonBase:
         await bot.send_log("SeasonalBot Loaded!", f"Active Season: **{self.name_clean}**")
 
 
-class SeasonManager:
+class SeasonManager(commands.Cog):
     """
     A cog for managing seasons.
     """
