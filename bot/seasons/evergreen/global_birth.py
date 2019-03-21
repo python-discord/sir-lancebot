b598bdd023f94037a47d097dd6f34a676aaf8e63
Olson Dimanche <dimanche.nav@gmail.com>
2019-03-21 15:23:24 -0400

    initial global info

--- bot/seasons/evergreen/global_birth.py
+++ bot/seasons/evergreen/global_birth.py
@@ -1,12 +1,10 @@
 import json
 import logging
 from pathlib import Path
-from random import choice
 
-import discord
+# import discord
 from discord.ext import commands
 
-from bot.constants import Colours
 
 log = logging.getLogger(__name__)
 
@@ -15,41 +13,24 @@ with open(Path("bot", "resources", "evergreen", "global_birth.json"), "r") as in
 
 
 class CountriesBirth:
+    """
+    Compilation of countries independence days, as well as some information about them
+    """
+
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(aliases=('birthdate'))
-    async def independence_date(self, ctx):
+    @commands.command()
+    async def inform(self, ctx, country_name: str = ""):
         """
-        Displays the country(s) whose independence date is today.
-        Displays the independence information about a particular country.
+        @returns: output the name and info on the country(ies) who's birthday is today.
+        @returns: output the independence information about the country provided
         """
-        # embed = discord.Embed(
-        #     title="Who is Saint Valentine?",
-        #     description=FACTS['whois'],
-        #     color=Colours.pink
-        # )
-        # embed.set_thumbnail(
-        #     url='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Saint_Valentine_-_'
-        #         'facial_reconstruction.jpg/1024px-Saint_Valentine_-_facial_reconstruction.jpg'
-        # )
+        output = f" The country is who knows"
+        if country_name:
+            output = f" The country is {country_name}"
 
-
-
-        await ctx.channel.send()
-
-    # @commands.command()
-    # async def valentine_fact(self, ctx):
-    #     """
-    #     Shows a random fact about Valentine's Day.
-    #     """
-    #     embed = discord.Embed(
-    #         title=choice(FACTS['titles']),
-    #         description=choice(FACTS['text']),
-    #         color=Colours.pink
-    #     )
-    #
-    #     await ctx.channel.send(embed=embed)
+        await ctx.send(output)
 
 
 def setup(bot):
