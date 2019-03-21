42d75f88211fecd8f72fadfec2b0ff44f1963d17
Olson Dimanche <dimanche.nav@gmail.com>
2019-03-21 17:10:14 -0400

    added logic for countries info, as well as countries json

--- bot/seasons/evergreen/global_birth.py
+++ bot/seasons/evergreen/global_birth.py
@@ -1,6 +1,7 @@
 import json
 import logging
 from pathlib import Path
+from datetime import datetime
 
 # import discord
 from discord.ext import commands
@@ -23,12 +24,28 @@ class CountriesBirth:
     @commands.command()
     async def inform(self, ctx, country_name: str = ""):
         """
-        @returns: output the name and info on the country(ies) who's birthday is today.
-        @returns: output the independence information about the country provided
+        @returns output the name and info on the country(ies) who's birthday is today.
+        @returns output the independence information about the country provided
         """
-        output = f" The country is who knows"
+        dates = indep_info["dates"]
+        countries = indep_info["countries"]
+
+        output = " The country is who knows"
         if country_name:
-            output = f" The country is {country_name}"
+            output = f" The country is who knows {country_name}, {len(countries)}"
+            if str(country_name) in countries:
+                name = countries[country_name][0]["name"]
+                # info = countries[country_name][0]["info"]
+                output = f" The country is {name}"
+        else:
+            today = datetime.now()
+            month = today.strftime("%B")
+            day = str(today.strftime("%d"))
+
+            if day in dates[month]:
+                output = []
+                for item in dates[month][day]:
+                    output.append(item)
 
         await ctx.send(output)
 
