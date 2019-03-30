f5237aca7a52dcf86a3b87da89dc71e816e849f6
Mark <kozlovmark@gmail.com>
2019-03-30 16:54:04 +1000

    Tidy capitalization and wording.
    Co-Authored-By: scragly <29337040+scragly@users.noreply.github.com>

--- bot/seasons/easter/__init__.py
+++ bot/seasons/easter/__init__.py
@@ -6,15 +6,15 @@ class Easter(SeasonBase):
     To celebrate, we have a lovely new colourful server icon. Thanks to <@140605665772175361> for
     the design.
 
-    Easter is a beautiful time of the year often celebrated after the first Full Moon of the
-    Northern Hemisphere's new spring seasonm, making it a beautiful time of the year with colorful
+    Easter is a beautiful time of the year often celebrated after the first full moon of the
+    Northern Hemisphere's new spring season, making it a beautiful time of the year with colorful
     flowers coming out to greet us.
 
-    We hope you can join us in the Spring and Easter celebrations by heading over to the
+    We hope you can join us in the spring and Easter celebrations by heading over to the
     [SeasonalBot GitHub repo](https://git.io/fjkvQ) and either submit some issues with ideas for
-    commands of this season, or to pick out an issue that you think looks fun to build yourself.
+    commands for this season, or to pick out an issue that you think looks fun to work on yourself.
 
-    Come over to <#542272993192050698> if you're interested or had any questions!
+    Come over to <#542272993192050698> if you're interested or have any questions!
     """
 
     name = "easter"
