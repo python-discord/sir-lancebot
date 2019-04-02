a8935821e96f61b4fe455acfbc5c5052449ec365
RohanRadia <rohan.radia@icloud.com>
2019-04-02 22:36:46 +0100

    Fixed PATH call

--- bot/seasons/easter/traditions.py
+++ bot/seasons/easter/traditions.py
@@ -7,27 +7,27 @@ from discord.ext import commands
 
 log = logging.getLogger(__name__)
 
-with open(Path('bot', 'resources', 'easter', 'traditions.json', 'r', encoding="utf8") as f:
-	traditions = json.load(f)
+with open(Path('bot', 'resources', 'easter', 'traditions.json'), 'r', encoding="utf8") as f:
+    traditions = json.load(f)
 
 
 class Traditions(commands.Cog):
-	"""A cog which allows users to get a random easter tradition or custom from a random country."""
+    """A cog which allows users to get a random easter tradition or custom from a random country."""
 
-	def __init__(self, bot):
-		self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
 
-	@commands.command(alias='eastercustoms')
-	async def easter_tradition(self, ctx):
-		"""Responds with a random tradition or custom"""
+    @commands.command(alias='eastercustoms')
+    async def easter_tradition(self, ctx):
+    """Responds with a random tradition or custom"""
 
-		random_country = random.choice(list(traditions))
+        random_country = random.choice(list(traditions))
 
-		await ctx.send(f"{random_country}:\n{traditions[random_country]}")
+        await ctx.send(f"{random_country}:\n{traditions[random_country]}")
 
 
 def setup(bot):
-	"""Traditions Cog load."""
+    """Traditions Cog load."""
 
-	bot.add_cog(Traditions(bot))
-	log.info("Traditions cog loaded")
+    bot.add_cog(Traditions(bot))
+    log.info("Traditions cog loaded")
