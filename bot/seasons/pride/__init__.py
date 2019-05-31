cd71cdf76620464a1fe4214f2b186382dcc33943
Leon Sandøy <leon.haland@gmail.com>
2019-05-31 11:49:37 +0200

    Update __init__.py

--- bot/seasons/pride/__init__.py
+++ bot/seasons/pride/__init__.py
@@ -3,9 +3,18 @@ from bot.seasons import SeasonBase
 
 class Pride(SeasonBase):
     """
-    No matter your origin, identity or sexuality, we come together to celebrate each and everyone's individuality.
+    It's important to us here at Python Discord that everyone feels welcome, no matter their origin, identity or sexuality. 
+    During the month of June, while some of you are participating in Pride festivals across the world, we will be celebrating 
+    individuality and commemorating the history and challenges of the LGBTQ+ community with a Pride event of our own!
 
-    Feature contributions to ProudBot is encouraged to commemorate the history and challenges of the LGBTQ+ community.
+    While this celebration takes place, you'll notice a few changes:
+    • The server icon has changed to our Pride icon. Thanks to <@98694745760481280> for the design!
+    • [Pride issues are now available for SeasonalBot on the repo](https://git.io/pythonpride).
+    • You may see Pride-themed esoteric challenges and other microevents.
+
+    If you'd like to contribute, head on over to <#542272993192050698> and we will help you get
+    started. It doesn't matter if you're new to open source or Python, if you'd like to help, we
+    will find you a task and teach you what you need to know.
     """
 
     name = "pride"
