d8d5cfe887a4ed1e8e328fc27424c6ff9bc9f80c
Leon Sandøy <leon.haland@gmail.com>
2019-05-31 12:17:48 +0200

    Update __init__.py

--- bot/seasons/pride/__init__.py
+++ bot/seasons/pride/__init__.py
@@ -3,9 +3,12 @@ from bot.seasons import SeasonBase
 
 class Pride(SeasonBase):
     """
-    It's important to us here at Python Discord that everyone feels welcome, no matter their origin, identity or sexuality. 
-    During the month of June, while some of you are participating in Pride festivals across the world, we will be celebrating 
-    individuality and commemorating the history and challenges of the LGBTQ+ community with a Pride event of our own!
+    The month of June is a special month for us at Python Discord.
+    
+    It is very important to us that everyone feels welcome here, no matter their origin,
+    identity or sexuality. During the month of June, while some of you are participating in Pride
+    festivals across the world, we will be celebrating individuality and commemorating the history
+    and challenges of the LGBTQ+ community with a Pride event of our own!
 
     While this celebration takes place, you'll notice a few changes:
     • The server icon has changed to our Pride icon. Thanks to <@98694745760481280> for the design!
