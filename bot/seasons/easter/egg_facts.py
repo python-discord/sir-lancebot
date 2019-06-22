1898c3cca4ea70767fe50bcd0020ad95fe295252
Rohan <icemam449@gmail.com>
2019-06-22 22:34:49 +0530

    removed blank lines after function doc strings

--- bot/seasons/easter/egg_facts.py
+++ bot/seasons/easter/egg_facts.py
@@ -20,7 +20,6 @@ class EasterFacts(commands.Cog):
 
     It also contains a background task which sends an easter egg fact in the event channel everyday.
     """
-
     def __init__(self, bot):
         self.bot = bot
         self.facts = self.load_json()
@@ -28,7 +27,6 @@ class EasterFacts(commands.Cog):
     @staticmethod
     def load_json():
         """Load a list of easter egg facts from the resource JSON file."""
-
         p = Path('bot', 'resources', 'easter', 'easter_egg_facts.json')
         with p.open(encoding="utf8") as f:
             facts = load(f)
@@ -36,7 +34,6 @@ class EasterFacts(commands.Cog):
 
     async def send_egg_fact_daily(self):
         """A background task that sends an easter egg fact in the event channel everyday."""
-
         channel = Channels.seasonalbot_chat
         while True:
             embed = self.make_embed()
@@ -46,13 +43,11 @@ class EasterFacts(commands.Cog):
     @commands.command(name='eggfact', aliases=['fact'])
     async def easter_facts(self, ctx):
         """Get easter egg facts."""
-
         embed = self.make_embed()
         await ctx.send(embed=embed)
 
     def make_embed(self):
         """Makes a nice embed for the message to be sent."""
-
         embed = discord.Embed()
         embed.colour = Colours.soft_red
         embed.title = 'Easter Egg Fact'
@@ -63,7 +58,6 @@ class EasterFacts(commands.Cog):
 
 def setup(bot):
     """Easter Egg facts loaded."""
-
     bot.loop.create_task(EasterFacts(bot).send_egg_fact_daily())
     bot.add_cog(EasterFacts(bot))
     log.info("EasterFacts cog loaded")
