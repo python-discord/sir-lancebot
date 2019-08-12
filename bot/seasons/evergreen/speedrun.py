12aefcf543ae5e2fb00ddc1dd0fbebadab0691d5
jakeHebert <jakehebert14@gmail.com>
2019-08-12 16:41:19 -0400

    Cleaned speedrun.py, removed duplicate in json file

--- bot/seasons/evergreen/speedrun.py
+++ bot/seasons/evergreen/speedrun.py
@@ -3,33 +3,27 @@ import logging
 from pathlib import Path
 from random import choice
 
-
 from discord.ext import commands
-
 
 log = logging.getLogger(__name__)
 
 
 class Speedrun(commands.Cog):
-    """A command that will link a random speedrun video from youtube to Discord."""
+    """Commands about the video game speedrunning community."""
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name="speedrun")
     async def get_speedrun(self, ctx):
-        """
-        Sends a link to Discord of a random speedrun from youtube.
-
-        Utilizes speedrun_links.json to find links.
-        """
+        """Sends a link to a video of a random speedrun."""
         with open(Path('bot/resources/evergreen/speedrun_links.json')) as file:
             data = json.load(file)
-        links = data['links']
-        await ctx.send(choice(links))
+        # links = data['links']
+        await ctx.send(choice(data))
 
 
 def setup(bot):
-    """Cog load"""
+    """Load the Speedrun cog"""
     bot.add_cog(Speedrun(bot))
     log.info("Speedrun cog loaded")
