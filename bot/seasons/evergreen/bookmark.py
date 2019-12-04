5414619068eb958b09fb78030f06b25c741a9b44
AG_1436 <45149585+AG4lyf@users.noreply.github.com>
2019-12-04 21:07:38 +0530

    Fixed grammatical errors, and simplified the code a bit.

--- bot/seasons/evergreen/bookmark.py
+++ bot/seasons/evergreen/bookmark.py
@@ -17,12 +17,9 @@ class Bookmark(commands.Cog):
         """Bookmarks a message."""
         if jump_url is not None and jump_url != "*":
             if "discordapp.com/channels" not in jump_url:
-                await ctx.send("INVALID URL")
-                await ctx.send("An example of the command can be \n"
-                               "!bm https://discordapp.com/channels"
-                               "/267624335836053506/267631170882240512/"
-                               "554639398130548746"
-                               " Some of the hints here")
+                await ctx.send("I can't find the associated message,"
+                               " the command support the following syntax:\n"
+                               " `.bm [message url] (bookmark name)`")
                 return
         if jump_url is None or jump_url == "*":
             channel = ctx.channel
@@ -32,26 +29,27 @@ class Bookmark(commands.Cog):
                 message = await channel.fetch_message(message_id)
             except discord.NotFound:
                 await ctx.send(">>> Message not in this"
-                               " channel\nOr\nYou are trying to Pin"
-                               " your own message,\nPlease use the channel"
+                               " channel\n"
+                               "Or\n"
+                               "You are trying to Pin"
+                               " your own message,\n"
+                               "Please use the channel"
                                " where message is.")
                 return
             jump_url = f"https://discordapp.com/channels/" \
                        f"{ctx.guild.id}/{channel.id}/{message.id}"
 
         embed = discord.Embed(
-            title="Your Bookmark",
-            description="You used the commands so here it is.",
+            title="Your bookmark",
+            description=None,
             colour=0x2ecc71
         )
-        hint = args
-        list(hint)
-        x = hint
+        x = args
         hint = ""
-        for a in x:
-            hint = hint + " " + a
+        for word in x:
+            hint = hint + " " + word
         if hint == "":
-            hint = "No hint Provided"
+            hint = "No hint provided"
 
         embed.set_footer(text="Why everything so heavy ?")
         embed.set_thumbnail(url="https://emojipedia-us.s3."
@@ -64,10 +62,10 @@ class Bookmark(commands.Cog):
         try:
             await ctx.author.send(embed=embed)
         except discord.Forbidden:
-            await ctx.send("Something not right ,"
+            await ctx.send("Something is not right,"
                            " you have either blocked me or"
-                           " you have disabled direct message "
-                           "message from this server.")
+                           " you have disabled direct messages "
+                           "from this server.")
             return
         await ctx.send("Sent you that DM")
 
