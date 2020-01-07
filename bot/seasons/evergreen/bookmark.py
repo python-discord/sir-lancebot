9735b0ad3bbfc98df5f48b0fe3e0c7fdf3c4be6c
AG_1436 <45149585+AG4lyf@users.noreply.github.com>
2020-01-07 20:30:44 +0530

    Moved jump url into a field.

--- bot/seasons/evergreen/bookmark.py
+++ bot/seasons/evergreen/bookmark.py
@@ -26,16 +26,12 @@ class Bookmark(commands.Cog):
         """Send the author a link to `target_message` via DMs."""
         log.info(f"{ctx.author} bookmarked {target_message.jump_url} with title '{title}'")
         content = target_message.content
-        if len(content) > 250:
-            content = content[:250]+'......'
         embed = discord.Embed(
             title=title,
             colour=Colours.soft_green,
-            description=(
-                f"{content}\n\n"
-                f"[Visit original message]({target_message.jump_url})"
-            )
+            description=content
         )
+        embed.add_field(name="Wanna give it a visit?", value=f"[Visit original message]({target_message.jump_url})")
         embed.set_author(name=target_message.author, icon_url=target_message.author.avatar_url)
         embed.set_thumbnail(url=bookmark_icon_url)
 
