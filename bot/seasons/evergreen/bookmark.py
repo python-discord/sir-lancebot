650b213225988ead0cd513d0ff72526379f838cd
AG_1436 <45149585+AG4lyf@users.noreply.github.com>
2020-02-27 12:19:58 +0530

    added exception pass for other exception while working on reaction.

--- bot/seasons/evergreen/bookmark.py
+++ bot/seasons/evergreen/bookmark.py
@@ -100,9 +100,13 @@ class Bookmark(commands.Cog):
                 else:
                     await user.send(embed=copied_embed)
 
-            except discord.Forbidden:  # Mention so that we can differentiate who it was for
+            except discord.Forbidden:
                 await ctx.send(f"{user.mention} Please enable your DM to receive the message.", delete_after=7.5)
                 await reaction.remove(user)
+            except discord.HTTPException:
+                pass
+            except discord.NotFound:
+                pass
             else:
                 sent_person.add(user)
 
