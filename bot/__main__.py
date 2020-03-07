db69310754e5cb2acbb246167542203836d86356
kwzrd <kwzrd@pythondiscord.com>
2020-03-07 13:31:34 +0100

    Deseasonify: load all extensions on start-up
    Extensions are no longer bound to their respective seasons.

--- bot/__main__.py
+++ bot/__main__.py
@@ -3,8 +3,13 @@ import logging
 from bot.bot import bot
 from bot.constants import Client, STAFF_ROLES, WHITELISTED_CHANNELS
 from bot.decorators import in_channel_check
+from bot.seasons import get_extensions
 
 log = logging.getLogger(__name__)
 
 bot.add_check(in_channel_check(*WHITELISTED_CHANNELS, bypass_roles=STAFF_ROLES))
+
+for ext in get_extensions():
+    bot.load_extension(ext)
+
 bot.run(Client.token)
