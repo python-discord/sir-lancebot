41e6a8db637ded63b590328fa99fd6973ac6e39c
kwzrd <kwzrd@pythondiscord.com>
2020-03-07 13:21:17 +0100

    Deseasonify: do not load legacy SeasonManager on start-up

--- bot/__main__.py
+++ bot/__main__.py
@@ -7,5 +7,4 @@ from bot.decorators import in_channel_check
 log = logging.getLogger(__name__)
 
 bot.add_check(in_channel_check(*WHITELISTED_CHANNELS, bypass_roles=STAFF_ROLES))
-bot.load_extension("bot.seasons")
 bot.run(Client.token)
