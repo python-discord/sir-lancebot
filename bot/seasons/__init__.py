3657fd856ba57687083d3bc541b41d8cd87b460c
kwzrd <kwzrd@pythondiscord.com>
2020-03-07 12:59:45 +0100

    Deseasonify: remove SeasonManager cog

--- bot/seasons/__init__.py
+++ bot/seasons/__init__.py
@@ -2,13 +2,8 @@ import logging
 
 from discord.ext import commands
 
-from bot.seasons.season import SeasonBase, SeasonManager, get_season
+from bot.seasons.season import SeasonBase, get_season
 
 __all__ = ("SeasonBase", "get_season")
 
 log = logging.getLogger(__name__)
-
-
-def setup(bot: commands.Bot) -> None:
-    bot.add_cog(SeasonManager(bot))
-    log.info("SeasonManager cog loaded")
