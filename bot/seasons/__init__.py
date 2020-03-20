1435a916bc760de40b2f000f97b1a7f11a9a9a12
kwzrd <kwzrd@pythondiscord.com>
2020-03-20 22:48:32 +0100

    Deseasonify: define all seasons in one place
    Defining seasons in seasonal packages' __init__.py files made more
    sense when extensions were strictly tied to seasons. It introduces an
    annoying circular dependency where a seasonal package must be imported
    in order for the __init__.py file to run and register the season, but
    it also imports SeasonBase from the parent directory so that it can
    inherit from it.
    
    I have made the decision to scrap the seasonal __init__.py files, and
    instead define all seasons directly under SeasonBase. The classes are
    no longer scattered around, we remove the above mentioned import
    problem, and everything is more transparent and easier to digest.

--- bot/seasons/__init__.py
+++ bot/seasons/__init__.py
@@ -6,9 +6,137 @@ from typing import List, Optional, Set, Type
 
 from bot.constants import Colours, Month
 
-__all__ = ("SeasonBase", "get_seasons", "get_extensions", "get_current_season", "get_season")
+__all__ = (
+    "SeasonBase",
+    "Christmas",
+    "Easter",
+    "Halloween",
+    "Pride",
+    "Valentines",
+    "Wildcard",
+    "get_seasons",
+    "get_extensions",
+    "get_current_season",
+    "get_season",
+)
 
 log = logging.getLogger(__name__)
+
+
+class SeasonBase:
+    """
+    Base for Seasonal classes.
+
+    This serves as the off-season fallback for when no specific
+    seasons are active.
+
+    Seasons are 'registered' simply by inheriting from `SeasonBase`.
+    We discover them by calling `__subclasses__`.
+    """
+
+    season_name: str = "Evergreen"
+    bot_name: str = "SeasonalBot"
+
+    colour: str = Colours.soft_green
+    description: str = "The default season!"
+
+    branding_path: str = "seasonal/evergreen"
+
+    months: Set[Month] = set(Month)
+
+
+class Christmas(SeasonBase):
+    """Branding for december."""
+
+    season_name = "Festive season"
+    bot_name = "Merrybot"
+
+    colour = Colours.soft_red
+    description = (
+        "The time is here to get into the festive spirit! No matter who you are, where you are, "
+        "or what beliefs you may follow, we hope every one of you enjoy this festive season!"
+    )
+
+    branding_path = "seasonal/christmas"
+
+    months = {Month.december}
+
+
+class Easter(SeasonBase):
+    """Branding for april."""
+
+    season_name = "Easter"
+    bot_name = "BunnyBot"
+
+    colour = Colours.bright_green
+    description = (
+        "Bunny here, bunny there, bunny everywhere! Here at Python Discord, we celebrate "
+        "our version of Easter during the entire month of April."
+    )
+
+    branding_path = "seasonal/easter"
+
+    months = {Month.april}
+
+
+class Halloween(SeasonBase):
+    """Branding for october."""
+
+    season_name = "Halloween"
+    bot_name = "NeonBot"
+
+    colour = Colours.orange
+    description = "Trick or treat?!"
+
+    branding_path = "seasonal/halloween"
+
+    months = {Month.october}
+
+
+class Pride(SeasonBase):
+    """Branding for june."""
+
+    season_name = "Pride"
+    bot_name = "ProudBot"
+
+    colour = Colours.pink
+    description = (
+        "The month of June is a special month for us at Python Discord. It is very important to us "
+        "that everyone feels welcome here, no matter their origin, identity or sexuality. During the "
+        "month of June, while some of you are participating in Pride festivals across the world, "
+        "we will be celebrating individuality and commemorating the history and challenges "
+        "of the LGBTQ+ community with a Pride event of our own!"
+    )
+
+    branding_path = "seasonal/pride"
+
+    months = {Month.june}
+
+
+class Valentines(SeasonBase):
+    """Branding for february."""
+
+    season_name = "Valentines"
+    bot_name = "TenderBot"
+
+    colour = Colours.pink
+    description = "Love is in the air!"
+
+    branding_path = "seasonal/valentines"
+
+    months = {Month.february}
+
+
+class Wildcard(SeasonBase):
+    """Branding for august."""
+
+    season_name = "Wildcard"
+    bot_name = "RetroBot"
+
+    colour = Colours.purple
+    description = "A season full of surprises!"
+
+    months = {Month.august}
 
 
 def get_seasons() -> List[str]:
@@ -42,28 +170,6 @@ def get_extensions() -> List[str]:
             extensions.append(f"bot.seasons.{package.name}")
 
     return extensions
-
-
-class SeasonBase:
-    """
-    Base for Seasonal classes.
-
-    This serves as the off-season fallback for when no specific
-    seasons are active.
-
-    Seasons are 'registered' simply by inheriting from `SeasonBase`.
-    We discover them by calling `__subclasses__`.
-    """
-
-    season_name: str = "Evergreen"
-    bot_name: str = "SeasonalBot"
-
-    colour: str = Colours.soft_green
-    description: str = "The default season!"
-
-    branding_path: str = "seasonal/evergreen"
-
-    months: Set[Month] = set(Month)
 
 
 def get_current_season() -> Type[SeasonBase]:
