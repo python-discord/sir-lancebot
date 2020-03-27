fe1bf965fc8bddb6d9563519cf939029088f3c96
creator1372 <ximons201@gmail.com>
2020-03-27 13:23:53 -0500

    Add a command to look for Game Jam 2020 teammates
    This adds a command that searches for teammates

---
+++ bot/seasons/evergreen/codejam.py
@@ -0,0 +1,37 @@
+import asyncio
+import random
+from typing import Union
+
+import discord
+from discord import User
+from discord.ext import commands
+
+
+class MatchMakingAlgo(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        self.matchmaking_pool = []
+
+    async def search_teammate(self) -> Union[User, None]:
+        if self.matchmaking_pool == []:
+            await asyncio.sleep(5)
+            return await self.search_teammate()
+        t = random.choice(self.matchmaking_pool())
+        return self.matchmaking_pool.pop(t, None)
+
+    @commands.command()
+    async def find_teammate(self, ctx: commands.Context) -> None:
+        self.matchmaking_pool.append(ctx.author)
+        embed = discord.Embed(title="Searcing for a teammate...",
+                              color=0xFF0000)
+        message: discord.Message = await ctx.send(embed=embed)
+        teammate = self.search_teammate()
+        embed = discord.Embed(title="Teammate found!",
+                              color=0x00FF00)
+        await message.edit(content=f"Hello {teammate.mention} and"
+                                   f"{ctx.user.mention}",
+                                   embed=embed)
+
+
+def setup(bot: commands.Bot) -> None:
+    bot.add_cog(MatchMakingAlgo(bot))
