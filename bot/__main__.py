b82d104130f6859bee620505b3ac04c3c0680600
kwzrd <kwzrd@pythondiscord.com>
2020-03-28 15:41:26 +0100

    Deseasonify: make `get_extensions` an iterator
    Simplify internal structure & adjust var naming.

--- bot/__main__.py
+++ bot/__main__.py
@@ -2,14 +2,14 @@ import logging
 
 from bot.bot import bot
 from bot.constants import Client, STAFF_ROLES, WHITELISTED_CHANNELS
-from bot.exts import get_extensions
+from bot.exts import walk_extensions
 from bot.utils.decorators import in_channel_check
 
 log = logging.getLogger(__name__)
 
 bot.add_check(in_channel_check(*WHITELISTED_CHANNELS, bypass_roles=STAFF_ROLES))
 
-for ext in get_extensions():
+for ext in walk_extensions():
     bot.load_extension(ext)
 
 bot.run(Client.token)
