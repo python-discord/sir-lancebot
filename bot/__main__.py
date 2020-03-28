09edf447aa3da21a14da0189025db6ef5e32ca42
kwzrd <kwzrd@pythondiscord.com>
2020-03-28 14:59:45 +0100

    Deseasonify: move branding extension under evergreen pkg

--- bot/__main__.py
+++ bot/__main__.py
@@ -12,6 +12,4 @@ bot.add_check(in_channel_check(*WHITELISTED_CHANNELS, bypass_roles=STAFF_ROLES))
 for ext in get_extensions():
     bot.load_extension(ext)
 
-bot.load_extension("bot.branding")
-
 bot.run(Client.token)
