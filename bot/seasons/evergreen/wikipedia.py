2dde7cd374651f5ec5bc04edab41c159d096d1f2
vivax <ole_egil@outlook.com>
2020-03-31 14:03:59 +0200

    got it working

--- bot/seasons/evergreen/wikipedia.py
+++ bot/seasons/evergreen/wikipedia.py
@@ -1,9 +1,12 @@
 import logging
-from typing import List
+from typing import Optional
 
 from discord.ext import commands
 
 log = logging.getLogger(__name__)
+
+SEARCH_API = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch={0}&format=json"
+WIKIPEDIA_URL = "https://en.wikipedia.org/wiki/{0}"
 
 
 class WikipediaCog(commands.Cog):
@@ -11,14 +14,32 @@ class WikipediaCog(commands.Cog):
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        self.http_session = bot.http_session
 
-    async def search_wikipedia(self, search_term: str) -> List[str]:
-        """Search wikipedia and return the pages found."""
+    async def search_wikipedia(self, search_term: str) -> Optional[str]:
+        """Search wikipedia and return the first page found."""
+        async with self.http_session.get(SEARCH_API.format(search_term)) as response:
+            data = await response.json()
+
+        search_result = data["query"]["search"]
+        if len(search_result) == 0:
+            return None
+
+        page = search_result[0]
+
+        # we dont like "may refere to" pages.
+        if "may refer to" in page["snippet"]:
+            log.info(f"hit a 'may refer to' page, taking result 2 (index 1)")
+            page = search_result[1]
+
+        return page["title"]
 
     @commands.command(name="wikipedia", aliases=["wiki"])
     async def wikipedia(self, ctx: commands.Context, *, search: str) -> None:
         """Search for something on wikipedia."""
-        await ctx.send(f"you searched for '{search}'")
+        title = await self.search_wikipedia(search)
+        title = title.replace(" ", "_")  # wikipedia uses "_" as spaces
+        await ctx.send(WIKIPEDIA_URL.format(title))
 
 
 def setup(bot: commands.Bot) -> None:
