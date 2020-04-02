b38fd5d027cadef659aa0da6cd3ece2ee984831f
Dhruv Duseja <dhruvduseja@gmail.com>
2020-04-02 23:54:17 +0400

    Made required changes

--- bot/seasons/evergreen/nationday.py
+++ bot/seasons/evergreen/nationday.py
@@ -1,8 +1,8 @@
-import datetime
 import json
 import logging
+from datetime import date
 from pathlib import Path
-from typing import List, Tuple
+from typing import List, Optional, Tuple
 
 import discord
 from aiohttp import ClientSession
@@ -13,14 +13,12 @@ from bot.pagination import ImagePaginator
 
 logger = logging.getLogger(__name__)
 # RESTful API to get countries info
-url = "https://restcountries.eu/rest/v2/alpha/"
+URL = "https://restcountries.eu/rest/v2/alpha/"
 # white flag image to show during error
-white_flag = (
+WHITE_FLAG = (
     "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/"
     "White_flag_waving.svg/158px-White_flag_waving.svg.png"
 )
-# url to get images of flags
-flag_url = "https://www.countryflags.io/{country_code}/flat/64.png"
 
 
 class NationDay(Cog):
@@ -35,25 +33,25 @@ class NationDay(Cog):
         with open(Path("bot/resources/evergreen/nationday/iso_codes.json"), "r") as file:
             self.iso_codes = json.load(file)
 
-    async def get_key(self, val: str) -> str:
+    async def get_date(self, val: str) -> Optional[str]:
         """Get date for particular country, if present."""
-        for key, value in self.by_day.items():
-            if val in value:
-                return key
+        for day, countries in self.by_day.items():
+            if val in countries:
+                return day
         return None
 
-    async def get_specific_country(self, country: str) -> Tuple[List[Tuple[str, str]], str]:
+    async def get_specific_country(self, country: str) -> Tuple[Tuple[str, str], str]:
         """
         Get Indepedence Day of given country.
 
         Return Indepedence Day and page of information on country.
         """
         # Get date of specified country from dict
-        date = await self.get_key(country)
+        date = await self.get_date(country)
         if date:
-            page, img = await self.get_country_info(self.http_session, country)
-            return [(page, img)], date
-        return [(None, None)], None
+            page, img = await self.get_country_info(country)
+            return (page, img), date
+        return (None, None), None
 
     async def country_today(self) -> List[Tuple[str, str]]:
         """
@@ -62,9 +60,9 @@ class NationDay(Cog):
         Return pages of info and flags of countries.
         """
         # Get current date [Month and day]
-        date = datetime.datetime.today()
-        month = date.strftime("%B")
-        day = date.day
+        today = date.today()
+        month = today.strftime("%B")
+        day = today.day
         today_date = f'{month} {day}'
         today_date = today_date.lower()
         try:
@@ -73,47 +71,51 @@ class NationDay(Cog):
         except KeyError as ke:
             err_msg = f"**No countries have their independence day today.** {ke}"
             logger.warning(err_msg)
-            return [(err_msg, white_flag)]
+            return [(err_msg, WHITE_FLAG)]
         # Create pages
         countries = list(countries.split(','))
         pages = []
         for country in countries:
-            page, img = await self.get_country_info(self.http_session, country)
+            page, img = await self.get_country_info(country)
             pages.append((page, img))
         return pages
 
-    async def get_country_info(self, client: ClientSession, country: str) -> Tuple[str, str]:
+    async def get_country_info(self, country: str) -> Tuple[str, str]:
         """
         Create country information page using RESTCountries API.
 
         Return page and flag image.
         """
-        try:
-            iso_code = self.iso_codes[country]
-            if iso_code is None:
-                not_available_msg = f"Could not find information on this country."
-                logger.warning(not_available_msg)
-                return not_available_msg, white_flag
-            async with client.get(url+iso_code) as resp:
-                info = await resp.json()
+        # url to get images of flags
+        flag_url = "https://www.countryflags.io/{country_code}/flat/64.png"
 
-            country_info = ""
-            country_info += f"**Name**: {info['name']}\n\n"
-            country_info += f"**Region**: {info['region']}\n\n"
-            country_info += f"**Capital**: {info['capital']}\n\n"
-            country_info += f"**Population**: {info['population']:,}\n\n"
-            country_info += f"**Currency**: {info['currencies'][0]['name']} ({info['currencies'][0]['symbol']})\n"
-            flag = flag_url.format(country_code=iso_code)
+        iso_code = self.iso_codes.get(country)
+        if iso_code is None:
+            not_available_msg = f"This country is not currently available."
+            logger.warning(not_available_msg)
+            return not_available_msg, WHITE_FLAG
 
-        except Exception as e:
-            msg = f"Error! {e}"
-            logger.warning(e)
-            return msg, white_flag
+        async with self.http_session.get(URL+iso_code) as resp:
+            info = await resp.json()
+
+        # check if 404 error
+        if info.get('status') == 404:
+            not_found_msg = f"Could not find information on this country."
+            logger.warning(not_found_msg)
+            return not_found_msg, WHITE_FLAG
+
+        country_info = ""
+        country_info += f"**Name**: {info['name']}\n"
+        country_info += f"**Region**: {info['region']}\n"
+        country_info += f"**Capital**: {info['capital']}\n"
+        country_info += f"**Population**: {info['population']:,}\n"
+        country_info += f"**Currency**: {info['currencies'][0]['name']} ({info['currencies'][0]['symbol']})\n"
+        flag = flag_url.format(country_code=iso_code)
 
         return country_info, flag
 
     @command(name='nationday')
-    async def nationday(self, ctx: Context, param: str = "") -> None:
+    async def nationday(self, ctx: Context, *, arg: str) -> None:
         """
         \U0001F30F NationDay Help.
 
@@ -123,15 +125,14 @@ class NationDay(Cog):
         -> .nationday today
         -> .nationday [country] (use "" for countries with space in the name)
         Examples:
-        -> .nationday india
-        -> .nationday colombia
         -> .nationday today
-        -> .nationday "United Arab Emirates"
-        -> .nationday "Ivory Coast"
+        -> .nationday india
+        -> .nationday United Arab Emirates
+        -> .nationday Ivory Coast
         """
-        param = param.lower()
+        arg = arg.lower()
 
-        if param == 'today':
+        if arg == 'today':
             pages = await self.country_today()
             embed = discord.Embed(
                 title='Countries that have their independence days today'
@@ -139,24 +140,27 @@ class NationDay(Cog):
             await ImagePaginator.paginate(pages, ctx, embed)
 
         # Check if country is present
-        elif param in self.iso_codes.keys():
-            page, date = await self.get_specific_country(param)
-            if page[0][0] is not None:
-                param_list = list(param.split(' '))
-                param_list = [x.capitalize() for x in param_list]
-                param = ' '.join(param_list)
-                embed = discord.Embed(
-                    title=f'{param} -> {date.capitalize()}'
-                ).set_footer(text='Powered by the RESTCountries API.')
-                await ImagePaginator.paginate(page, ctx, embed)
+        elif arg in self.iso_codes.keys():
+            page, date = await self.get_specific_country(arg)
+            embed = discord.Embed(
+                title=f'{arg.title()} -> {date.capitalize()}',
+                description=page[0]
+            ).set_footer(text='Powered by the RESTCountries API.')
+            embed.set_image(url=page[1])
+            await ctx.channel.send(embed=embed)
 
         else:
-            await ctx.channel.send("Give appropriate country name Eg. 'United States of America'\
-            \n**Country entered may not be available** OR an invalid option was used.\
-            \nCheck out the help section below.")
+            await ctx.channel.send(
+                (
+                    "Give appropriate country name Eg. 'United States of America'\n"
+                    "**Country entered may not be available** OR an invalid option was used.\n"
+                    "Check out the help section below."
+                )
+            )
             await ctx.send_help('nationday')
 
 
 def setup(bot: Bot) -> None:
     """Load NationDay Cog."""
     bot.add_cog(NationDay(bot))
+    logger.debug("NationDay cog loaded.")
