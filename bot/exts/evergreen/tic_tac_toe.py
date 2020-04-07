b6dc1207fd63564bee9814ca46a955c1067fe5db
ks129 <45097959+ks129@users.noreply.github.com>
2020-04-07 11:06:48 +0300

    (TicTacToe): Created `Player` class

--- bot/exts/evergreen/tic_tac_toe.py
+++ bot/exts/evergreen/tic_tac_toe.py
@@ -1,6 +1,14 @@
+import discord
 from discord.ext.commands import Cog
 
 from bot.bot import SeasonalBot
+
+
+class Player:
+    """Class that contains information about player and functions that interact with player."""
+
+    def __init__(self, user: discord.User):
+        self.user = user
 
 
 class TicTacToe(Cog):
