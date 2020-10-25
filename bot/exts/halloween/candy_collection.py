675900901c333b747d43806c9a525290886bbc14
Quanta <40241646+quanta-kt@users.noreply.github.com>
2020-10-25 21:40:41 +0530

    Typo in word "none"
    Co-authored-by: gustavwilliam <65498475+gustavwilliam@users.noreply.github.com>

--- bot/exts/halloween/candy_collection.py
+++ bot/exts/halloween/candy_collection.py
@@ -154,7 +154,7 @@ class CandyCollection(commands.Cog):
         """An alternative spooky message sent when user has no candies in the collection."""
         embed = discord.Embed(color=author.color)
         embed.set_author(name="Ghosts and Ghouls and Jack o' lanterns at night; "
-                              "I tried to take your candies but you had non to begin with!")
+                              "I tried to take your candies but you had none to begin with!")
         await channel.send(embed=embed)
 
     def save_to_json(self) -> None:
