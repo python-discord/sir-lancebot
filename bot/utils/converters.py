04f122348eca25dbbf44afdcc6fcd417aa98bf89
Rohan <rohanjnr44@gmail.com>
2020-12-02 21:53:29 +0530

    Add Subreddit converter.

--- bot/utils/converters.py
+++ bot/utils/converters.py
@@ -1,5 +1,6 @@
 import discord
-from discord.ext.commands.converter import MessageConverter
+from discord.ext.commands import BadArgument, Context
+from discord.ext.commands.converter import Converter, MessageConverter
 
 
 class WrappedMessageConverter(MessageConverter):
@@ -14,3 +15,32 @@ class WrappedMessageConverter(MessageConverter):
             argument = argument[1:-1]
 
         return await super().convert(ctx, argument)
+
+
+class Subreddit(Converter):
+    """Forces a string to begin with "r/" and checks if it's a valid subreddit."""
+
+    @staticmethod
+    async def convert(ctx: Context, sub: str) -> str:
+        """
+        Force sub to begin with "r/" and check if it's a valid subreddit.
+
+        If sub is a valid subreddit, return it prepended with "r/"
+        """
+        sub = sub.lower()
+
+        if not sub.startswith("r/"):
+            sub = f"r/{sub}"
+
+        resp = await ctx.bot.http_session.get(
+            "https://www.reddit.com/subreddits/search.json",
+            params={"q": sub}
+        )
+
+        json = await resp.json()
+        if not json["data"]["children"]:
+            raise BadArgument(
+                f"The subreddit `{sub}` either doesn't exist, or it has no posts."
+            )
+
+        return sub
