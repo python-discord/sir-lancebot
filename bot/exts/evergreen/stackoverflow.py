b6b271dd5913a0dd978af5d04b20cdc54b88afcd
Vivaan Parashar <parashar.xtra@gmail.com>
2021-02-11 14:12:50 +0530

    Apply cooldown to all users instead of global cooldown.

--- bot/exts/evergreen/stackoverflow.py
+++ bot/exts/evergreen/stackoverflow.py
@@ -15,7 +15,7 @@ class Stackoverflow(commands.Cog):
         self.bot = bot
 
     @commands.command(name="stackoverflow", aliases=["so"])
-    @commands.cooldown(1, 15)
+    @commands.cooldown(1, 15, commands.cooldowns.BucketType.user)
     async def stackoverflow(self, ctx: commands.Context, *, search_query: str) -> None:
         """Sends the top 5 results from stackoverflow based on a search query."""
         async with self.bot.http_session.get(BASE_URL.format(query=quote_plus(search_query))) as response:
