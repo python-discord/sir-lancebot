e89a0c500be705af1e842bf4fc8980689d364b6b
melodicht <marvincht@gmail.com>
2021-02-13 21:02:19 +0800

    Revert changes made to `pagination.py`
    Out of the scope of this PR.

--- bot/utils/pagination.py
+++ bot/utils/pagination.py
@@ -1,9 +1,8 @@
 import asyncio
 import logging
-from contextlib import suppress
-from typing import List, Optional, Tuple
+from typing import Iterable, List, Optional, Tuple
 
-from discord import Embed, Member, Message, NotFound, Reaction
+from discord import Embed, Member, Reaction
 from discord.abc import User
 from discord.ext.commands import Context, Paginator
 
@@ -27,36 +26,18 @@ class EmptyPaginatorEmbed(Exception):
 class LinePaginator(Paginator):
     """A class that aids in paginating code blocks for Discord messages."""
 
-    def __init__(
-        self,
-        prefix: str = '```',
-        suffix: str = '```',
-        max_size: int = 2000,
-        scale_to_size: int = 2000,
-        max_lines: Optional[int] = None
-    ) -> None:
+    def __init__(self, prefix: str = '```', suffix: str = '```', max_size: int = 2000, max_lines: int = None):
         """
-        This function overrides the Paginator.__init__ from inside discord.ext.commands.
+        Overrides the Paginator.__init__ from inside discord.ext.commands.
 
-        It overrides in order to allow us to configure the maximum number of lines per page.
+        `prefix` and `suffix` will be prepended and appended respectively to every page.
+
+        `max_size` and `max_lines` denote the maximum amount of codepoints and lines
+        allowed per page.
         """
         self.prefix = prefix
         self.suffix = suffix
-
-        # Embeds that exceed 2048 characters will result in an HTTPException
-        # (Discord API limit), so we've set a limit of 2000
-        if max_size > 2000:
-            raise ValueError(f"max_size must be <= 2,000 characters. ({max_size} > 2000)")
-
         self.max_size = max_size - len(suffix)
-
-        if scale_to_size < max_size:
-            raise ValueError(f"scale_to_size must be >= max_size. ({scale_to_size} < {max_size})")
-
-        if scale_to_size > 2000:
-            raise ValueError(f"scale_to_size must be <= 2,000 characters. ({scale_to_size} > 2000)")
-
-        self.scale_to_size = scale_to_size - len(suffix)
         self.max_lines = max_lines
         self._current_page = [prefix]
         self._linecount = 0
@@ -67,38 +48,24 @@ class LinePaginator(Paginator):
         """
         Adds a line to the current page.
 
-        If a line on a page exceeds `max_size` characters, then `max_size` will go up to
-        `scale_to_size` for a single line before creating a new page for the overflow words. If it
-        is still exceeded, the excess characters are stored and placed on the next pages until
-        there are none remaining (by word boundary). The line is truncated if `scale_to_size` is
-        still exceeded after attempting to continue onto the next page.
+        If the line exceeds the `max_size` then a RuntimeError is raised.
 
-        In the case that the page already contains one or more lines and the new lines would cause
-        `max_size` to be exceeded, a new page is created. This is done in order to make a best
-        effort to avoid breaking up single lines across pages, while keeping the total length of the
-        page at a reasonable size.
+        Overrides the Paginator.add_line from inside discord.ext.commands in order to allow
+        configuration of the maximum number of lines per page.
 
-        This function overrides the `Paginator.add_line` from inside `discord.ext.commands`.
-
-        It overrides in order to allow us to configure the maximum number of lines per page.
+        If `empty` is True, an empty line will be placed after the a given `line`.
         """
-        remaining_words = None
-        if len(line) > (max_chars := self.max_size - len(self.prefix) - 2):
-            if len(line) > self.scale_to_size:
-                line, remaining_words = self._split_remaining_words(line, max_chars)
-                if len(line) > self.scale_to_size:
-                    log.debug("Could not continue to next page, truncating line.")
-                    line = line[:self.scale_to_size]
+        if len(line) > self.max_size - len(self.prefix) - 2:
+            raise RuntimeError('Line exceeds maximum page size %s' % (self.max_size - len(self.prefix) - 2))
 
-        # Check if we should start a new page or continue the line on the current one
-        if self.max_lines is not None and self._linecount >= self.max_lines:
-            log.debug("max_lines exceeded, creating new page.")
-            self._new_page()
-        elif self._count + len(line) + 1 > self.max_size and self._linecount > 0:
-            log.debug("max_size exceeded on page with lines, creating new page.")
-            self._new_page()
+        if self.max_lines is not None:
+            if self._linecount >= self.max_lines:
+                self._linecount = 0
+                self.close_page()
 
-        self._linecount += 1
+            self._linecount += 1
+        if self._count + len(line) + 1 > self.max_size:
+            self.close_page()
 
         self._count += len(line) + 1
         self._current_page.append(line)
@@ -107,101 +74,31 @@ class LinePaginator(Paginator):
             self._current_page.append('')
             self._count += 1
 
-        # Start a new page if there were any overflow words
-        if remaining_words:
-            self._new_page()
-            self.add_line(remaining_words)
-
-    def _new_page(self) -> None:
-        """
-        Internal: start a new page for the paginator.
-
-        This closes the current page and resets the counters for the new page's line count and
-        character count.
-        """
-        self._linecount = 0
-        self._count = len(self.prefix) + 1
-        self.close_page()
-
-    def _split_remaining_words(
-        self,
-        line: str,
-        max_chars: int
-    ) -> Tuple[str, Optional[str]]:
-        """
-        Internal: split a line into two strings -- reduced_words and remaining_words.
-
-        reduced_words: the remaining words in `line`, after attempting to remove all words that
-            exceed `max_chars` (rounding down to the nearest word boundary).
-
-        remaining_words: the words in `line` which exceed `max_chars`. This value is None if
-            no words could be split from `line`.
-
-        If there are any remaining_words, an ellipses is appended to reduced_words and a
-        continuation header is inserted before remaining_words to visually communicate the line
-        continuation.
-
-        Return a tuple in the format (reduced_words, remaining_words).
-        """
-        reduced_words = []
-        remaining_words = []
-
-        # "(Continued)" is used on a line by itself to indicate the continuation of last page
-        continuation_header = "(Continued)\n-----------\n"
-        reduced_char_count = 0
-        is_full = False
-
-        for word in line.split(" "):
-            if not is_full:
-                if len(word) + reduced_char_count <= max_chars:
-                    reduced_words.append(word)
-                    reduced_char_count += len(word) + 1
-                else:
-                    # If reduced_words is empty, we were unable to split the words across pages
-                    if not reduced_words:
-                        return line, None
-                    is_full = True
-                    remaining_words.append(word)
-            else:
-                remaining_words.append(word)
-
-        return (
-            " ".join(reduced_words) + "..." if remaining_words else "",
-            continuation_header + " ".join(remaining_words) if remaining_words else None
-        )
-
     @classmethod
-    async def paginate(
-        cls,
-        lines: List[str],
-        ctx: Context,
-        embed: Embed,
-        prefix: str = "",
-        suffix: str = "",
-        max_lines: Optional[int] = None,
-        max_size: int = 500,
-        scale_to_size: int = 2000,
-        empty: bool = True,
-        restrict_to_user: User = None,
-        timeout: int = 300,
-        footer_text: str = None,
-        url: str = None,
-        exception_on_empty_embed: bool = False
-    ) -> Optional[Message]:
+    async def paginate(cls, lines: Iterable[str], ctx: Context, embed: Embed,
+                       prefix: str = "", suffix: str = "", max_lines: Optional[int] = None,
+                       max_size: int = 500, empty: bool = True, restrict_to_user: User = None,
+                       timeout: int = 300, footer_text: str = None, url: str = None,
+                       exception_on_empty_embed: bool = False):
         """
         Use a paginator and set of reactions to provide pagination over a set of lines.
 
         The reactions are used to switch page, or to finish with pagination.
 
-        When used, this will send a message using `ctx.send()` and apply a set of reactions to it. These reactions may
-        be used to change page, or to remove pagination from the message.
+        When used, this will send a message using `ctx.send()` and apply a set of reactions to it.
+        These reactions may be used to change page, or to remove pagination from the message.
 
-        Pagination will also be removed automatically if no reaction is added for five minutes (300 seconds).
+        Pagination will also be removed automatically if no reaction is added for `timeout` seconds,
+        defaulting to five minutes (300 seconds).
 
-        Example:
+        If `empty` is True, an empty line will be placed between each given line.
+
         >>> embed = Embed()
         >>> embed.set_author(name="Some Operation", url=url, icon_url=icon)
-        >>> await LinePaginator.paginate([line for line in lines], ctx, embed)
+        >>> await LinePaginator.paginate(
+        ...     (line for line in lines),
+        ...     ctx, embed
+        ... )
         """
         def event_check(reaction_: Reaction, user_: Member) -> bool:
             """Make sure that this reaction is what we want to operate on."""
@@ -218,7 +115,7 @@ class LinePaginator(Paginator):
                     # Reaction is on this message
                     reaction_.message.id == message.id,
                     # Reaction is one of the pagination emotes
-                    str(reaction_.emoji) in PAGINATION_EMOJI,
+                    str(reaction_.emoji) in PAGINATION_EMOJI,  # Note: DELETE_EMOJI is a string and not unicode
                     # Reaction was not made by the Bot
                     user_.id != ctx.bot.user.id,
                     # There were no restrictions
@@ -226,8 +123,7 @@ class LinePaginator(Paginator):
                 ))
             )
 
-        paginator = cls(prefix=prefix, suffix=suffix, max_size=max_size, max_lines=max_lines,
-                        scale_to_size=scale_to_size)
+        paginator = cls(prefix=prefix, suffix=suffix, max_size=max_size, max_lines=max_lines)
         current_page = 0
 
         if not lines:
@@ -291,7 +187,7 @@ class LinePaginator(Paginator):
                 log.debug("Timed out waiting for a reaction")
                 break  # We're done, no reactions for the last 5 minutes
 
-            if str(reaction.emoji) == DELETE_EMOJI:
+            if str(reaction.emoji) == DELETE_EMOJI:  # Note: DELETE_EMOJI is a string and not unicode
                 log.debug("Got delete reaction")
                 return await message.delete()
 
@@ -301,6 +197,8 @@ class LinePaginator(Paginator):
 
                 log.debug(f"Got first page reaction - changing to page 1/{len(paginator.pages)}")
 
+                embed.description = ""
+                await message.edit(embed=embed)
                 embed.description = paginator.pages[current_page]
                 if footer_text:
                     embed.set_footer(text=f"{footer_text} (Page {current_page + 1}/{len(paginator.pages)})")
@@ -314,6 +212,8 @@ class LinePaginator(Paginator):
 
                 log.debug(f"Got last page reaction - changing to page {current_page + 1}/{len(paginator.pages)}")
 
+                embed.description = ""
+                await message.edit(embed=embed)
                 embed.description = paginator.pages[current_page]
                 if footer_text:
                     embed.set_footer(text=f"{footer_text} (Page {current_page + 1}/{len(paginator.pages)})")
@@ -331,6 +231,8 @@ class LinePaginator(Paginator):
                 current_page -= 1
                 log.debug(f"Got previous page reaction - changing to page {current_page + 1}/{len(paginator.pages)}")
 
+                embed.description = ""
+                await message.edit(embed=embed)
                 embed.description = paginator.pages[current_page]
 
                 if footer_text:
@@ -350,6 +252,8 @@ class LinePaginator(Paginator):
                 current_page += 1
                 log.debug(f"Got next page reaction - changing to page {current_page + 1}/{len(paginator.pages)}")
 
+                embed.description = ""
+                await message.edit(embed=embed)
                 embed.description = paginator.pages[current_page]
 
                 if footer_text:
@@ -359,9 +263,8 @@ class LinePaginator(Paginator):
 
                 await message.edit(embed=embed)
 
-        log.debug("Ending pagination and clearing reactions.")
-        with suppress(NotFound):
-            await message.clear_reactions()
+        log.debug("Ending pagination and clearing reactions...")
+        await message.clear_reactions()
 
 
 class ImagePaginator(Paginator):
