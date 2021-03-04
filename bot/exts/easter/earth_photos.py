825080e805787afc1f36a80b4fa3f73040ebdc75
Kronifer <44979306+Kronifer@users.noreply.github.com>
2021-03-03 19:05:37 -0600

    Format change
    Co-authored-by: Shivansh-007 <69356296+Shivansh-007@users.noreply.github.com>

--- bot/exts/easter/earth_photos.py
+++ bot/exts/easter/earth_photos.py
@@ -19,7 +19,8 @@ class EarthPhotos(commands.Cog):
         async with ctx.typing():
             async with self.bot.http_session.get(
                     'https://api.unsplash.com/photos/random',
-                    params={"query": "earth", "client_id": UnClient_id}) as r:
+                    params={"query": "earth", "client_id": UnClient_id}
+            ) as r:
                 jsondata = await r.json()
                 linksdata = jsondata.get("urls")
                 embedlink = linksdata.get("regular")
