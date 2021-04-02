97a1747c22fbc96f7e3d1dc95209b74e6ff987dd
Boris Muratov <8bee278@gmail.com>
2021-04-03 02:24:00 +0300

    Move the operator to the next line

--- bot/exts/evergreen/issues.py
+++ bot/exts/evergreen/issues.py
@@ -181,8 +181,8 @@ class Issues(commands.Cog):
     async def on_message(self, message: discord.Message) -> None:
         """Command to retrieve issue(s) from a GitHub repository using automatic linking if matching <repo>#<issue>."""
         if not(
-            (hasattr(message.channel, "category") and
-             message.channel.category and message.channel.category.id in WHITELISTED_CATEGORIES)
+            (hasattr(message.channel, "category")
+             and message.channel.category and message.channel.category.id in WHITELISTED_CATEGORIES)
             or message.channel.id in WHITELISTED_CHANNELS_ON_MESSAGE
         ):
             return
