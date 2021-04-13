30187743fb86d054f2e9c94193448a00800162d6
Shivansh-007 <shivansh-007@outlook.com>
2021-04-13 16:37:54 +0530

    Adds super class init call.
    - Adds `linesep` parameter to LinePaginator.

--- bot/utils/pagination.py
+++ bot/utils/pagination.py
@@ -26,7 +26,14 @@ class EmptyPaginatorEmbed(Exception):
 class LinePaginator(Paginator):
     """A class that aids in paginating code blocks for Discord messages."""
 
-    def __init__(self, prefix: str = '```', suffix: str = '```', max_size: int = 2000, max_lines: int = None):
+    def __init__(
+            self,
+            prefix: str = '```',
+            suffix: str = '```',
+            max_size: int = 2000,
+            max_lines: int = None,
+            linesep: str = "\n"
+    ):
         """
         Overrides the Paginator.__init__ from inside discord.ext.commands.
 
@@ -35,10 +42,13 @@ class LinePaginator(Paginator):
         `max_size` and `max_lines` denote the maximum amount of codepoints and lines
         allowed per page.
         """
-        self.prefix = prefix
-        self.suffix = suffix
-        self.max_size = max_size - len(suffix)
-        self.max_lines = max_lines
+        super().__init__(
+            prefix,
+            suffix,
+            max_size - len(suffix),
+            linesep
+        )
+
         self._current_page = [prefix]
         self._linecount = 0
         self._count = len(prefix) + 1  # prefix + newline
