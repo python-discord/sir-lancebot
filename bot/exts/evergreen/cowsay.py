7cb229a49a497c04ec121c6703cc57eb691e0275
Kronifer <dillonr5@live.wsd1.org>
2021-04-21 14:03:02 +0000

    Use char_names.join() for docs

--- bot/exts/evergreen/cowsay.py
+++ bot/exts/evergreen/cowsay.py
@@ -1,6 +1,6 @@
-from random import choice
+import random
 
-from cowsay import get_output_string
+from cowsay import get_output_string, char_names
 from discord import Embed
 from discord.ext import commands
 
@@ -13,34 +13,20 @@ class Cowsay(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.command(aliases=["cow"])
-    async def cowsay(self, ctx: commands.Context, character: str = "Cow", *, text: str = "I'm a Cow") -> None:
+    @commands.command(aliases=["cow"], help=f"{char_names.join()}")
+    async def cowsay(self, ctx: commands.Context, character: str = "Cow", *, text: str = None) -> None:
         """
         Generates some cowsay ASCII art and sends it in Discord.
 
         There are multiple character options to choose from. Here is a list:
-
-        Cow
-        Dragon
-        Cheese
-        Beavis
-        Daemon
-        Ghostbusters
-        Kitty
-        Meow
-        Milk
-        Pig
-        Stegosaurus
-        Stimpy
-        Turkey
-        Turtle
-        Tux
         """
+        if not text:
+            text = f"I'm a {character.lower()}"
         text = text.lower()
         character = character.lower()
         if len(text) >= 150:
             embed = Embed(
-                title=choice(NEGATIVE_REPLIES),
+                title=random.choice(NEGATIVE_REPLIES),
                 description="The given text is too long! Please submit something under 150 characters.",
                 color=Colours.soft_red
             )
@@ -50,8 +36,16 @@ class Cowsay(commands.Cog):
             msgbody = get_output_string(character, text)
         except Exception:
             embed = Embed(
-                title=choice(NEGATIVE_REPLIES),
+                title=random.choice(NEGATIVE_REPLIES),
                 description="That is an invalid character! Please enter a valid one.",
+                color=Colours.soft_red
+            )
+            await ctx.send(embed=embed)
+            return
+        if character in ["dragon", "trex", "stegosaurus"]:
+            embed = Embed(
+                title=random.choice(NEGATIVE_REPLIES),
+                description="The given character is invalid! Please submit a valid character.",
                 color=Colours.soft_red
             )
             await ctx.send(embed=embed)
