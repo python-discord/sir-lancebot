4e0210922ef8d25eeb18f080c9baab022d70d0d2
Salil Chincholikar <31334826+chincholikarsalil@users.noreply.github.com>
2021-04-27 19:12:28 +0530

    Updated bot/utils/exceptions.py
    Co-authored-by: Joe Banks <joseph@josephbanks.me>

--- bot/utils/exceptions.py
+++ bot/utils/exceptions.py
@@ -1,4 +1,4 @@
 class UserNotPlayingError(Exception):
-    """Will be raised when the users try to use game commands when they are not playing."""
+    """Raised when users try to use game commands when they are not playing."""
 
     pass
