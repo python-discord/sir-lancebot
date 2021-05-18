79aafa6186d9ce0c381e91d84d57f6dc8dd9637b
Kronifer <44979306+Kronifer@users.noreply.github.com>
2021-05-18 09:12:56 -0500

    Changes requested by Shivansh-007

--- bot/exts/evergreen/cowsay.py
+++ bot/exts/evergreen/cowsay.py
@@ -8,23 +8,20 @@ from discord.ext import commands
 from bot.bot import Bot
 
 # Creates a local copy of char_names to filter out unsupported characters.
-LOCAL_CHAR_NAMES = list(char_names)
+SUPPORTED_CHAR_NAMES = list(char_names)
 for element in ["dragon", "trex", "stegosaurus", "turkey", "ghostbusters"]:
-    LOCAL_CHAR_NAMES.remove(element)
+    SUPPORTED_CHAR_NAMES.remove(element)
 
 
 class Cowsay(commands.Cog):
     """Cog for the cowsay command."""
-
-    def __init__(self, bot: Bot):
-        self.bot = bot
 
     @commands.command(
         aliases=["cow"],
         help=f"""
         Generates a cowsay string and sends it. The available characters for cowsay are:
 
-        {', '.join(LOCAL_CHAR_NAMES)}"""
+        {', '.join(SUPPORTED_CHAR_NAMES)}"""
     )
     async def cowsay(self, ctx: commands.Context, character: str = "Cow", *, text: Optional[str]) -> None:
         """Builds a cowsay string and sends it to Discord."""
@@ -34,7 +31,7 @@ class Cowsay(commands.Cog):
         if len(text) >= 150:
             raise commands.BadArgument("The given text is too long! Please submit something under 150 characters.")
 
-        if character not in LOCAL_CHAR_NAMES:
+        if character not in SUPPORTED_CHAR_NAMES:
             raise commands.BadArgument("The given character cannot be used! Please enter a valid character.")
         else:
             msgbody = get_output_string(character, text)
