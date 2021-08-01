a533a2f86bf1eea91bab0b0dc41c0c41ef93676d
melodicht <marvincht@gmail.com>
2021-08-01 18:19:07 +0800

    Update pagination

--- bot/utils/pagination.py
+++ bot/utils/pagination.py
@@ -1,13 +1,16 @@
 import asyncio
 import logging
+import typing as t
 from contextlib import suppress
-from typing import List, Optional, Tuple
+from functools import partial
 
-from discord import Embed, Member, Message, NotFound, Reaction
+import discord
 from discord.abc import User
 from discord.ext.commands import Context, Paginator
 
 from bot.constants import Emojis
+from bot.utils import messages
+
 
 FIRST_EMOJI = "\u23EE"   # [:track_previous:]
 LEFT_EMOJI = "\u2B05"    # [:arrow_left:]
@@ -20,41 +23,60 @@ PAGINATION_EMOJI = (FIRST_EMOJI, LEFT_EMOJI, RIGHT_EMOJI, LAST_EMOJI, DELETE_EMO
 log = logging.getLogger(__name__)
 
 
-class EmptyPaginatorEmbed(Exception):
-    """Base Exception class for an empty paginator embed."""
+class EmptyPaginatorEmbedError(Exception):
+    """Raised when attempting to paginate with empty contents."""
+
+    pass
 
 
 class LinePaginator(Paginator):
-    """A class that aids in paginating code blocks for Discord messages."""
+    """
+    A class that aids in paginating code blocks for Discord messages.
+
+    Available attributes include:
+    * prefix: `str`
+        The prefix inserted to every page. e.g. three backticks.
+    * suffix: `str`
+        The suffix appended at the end of every page. e.g. three backticks.
+    * max_size: `int`
+        The maximum amount of codepoints allowed in a page.
+    * scale_to_size: `int`
+        The maximum amount of characters a single line can scale up to.
+    * max_lines: `int`
+        The maximum amount of lines allowed in a page.
+    """
 
     def __init__(
         self,
         prefix: str = '```',
         suffix: str = '```',
-        max_size: int = 2000,
-        scale_to_size: int = 2000,
-        max_lines: Optional[int] = None
+        max_size: int = 4000,
+        scale_to_size: int = 4000,
+        max_lines: t.Optional[int] = None,
+        linesep: str = "\n"
     ) -> None:
         """
         This function overrides the Paginator.__init__ from inside discord.ext.commands.
 
         It overrides in order to allow us to configure the maximum number of lines per page.
         """
-        self.prefix = prefix
-        self.suffix = suffix
+        # Embeds that exceed 4096 characters will result in an HTTPException
+        # (Discord API limit), so we've set a limit of 4000
+        if max_size > 4000:
+            raise ValueError(f"max_size must be <= 4,000 characters. ({max_size} > 4000)")
 
-        # Embeds that exceed 2048 characters will result in an HTTPException
-        # (Discord API limit), so we've set a limit of 2000
-        if max_size > 2000:
-            raise ValueError(f"max_size must be <= 2,000 characters. ({max_size} > 2000)")
-
-        self.max_size = max_size - len(suffix)
+        super().__init__(
+            prefix,
+            suffix,
+            max_size - len(suffix),
+            linesep
+        )
 
         if scale_to_size < max_size:
             raise ValueError(f"scale_to_size must be >= max_size. ({scale_to_size} < {max_size})")
 
-        if scale_to_size > 2000:
-            raise ValueError(f"scale_to_size must be <= 2,000 characters. ({scale_to_size} > 2000)")
+        if scale_to_size > 4000:
+            raise ValueError(f"scale_to_size must be <= 2,000 characters. ({scale_to_size} > 4000)")
 
         self.scale_to_size = scale_to_size - len(suffix)
         self.max_lines = max_lines
@@ -69,17 +91,14 @@ class LinePaginator(Paginator):
 
         If a line on a page exceeds `max_size` characters, then `max_size` will go up to
         `scale_to_size` for a single line before creating a new page for the overflow words. If it
-        is still exceeded, the excess characters are stored and placed on the next pages until
+        is still exceeded, the excess characters are stored and placed on the next pages unti
         there are none remaining (by word boundary). The line is truncated if `scale_to_size` is
         still exceeded after attempting to continue onto the next page.
-
         In the case that the page already contains one or more lines and the new lines would cause
         `max_size` to be exceeded, a new page is created. This is done in order to make a best
         effort to avoid breaking up single lines across pages, while keeping the total length of the
         page at a reasonable size.
-
         This function overrides the `Paginator.add_line` from inside `discord.ext.commands`.
-
         It overrides in order to allow us to configure the maximum number of lines per page.
         """
         remaining_words = None
@@ -123,24 +142,17 @@ class LinePaginator(Paginator):
         self._count = len(self.prefix) + 1
         self.close_page()
 
-    def _split_remaining_words(
-        self,
-        line: str,
-        max_chars: int
-    ) -> Tuple[str, Optional[str]]:
+    def _split_remaining_words(self, line: str, max_chars: int) -> t.Tuple[str, t.Optional[str]]:
         """
         Internal: split a line into two strings -- reduced_words and remaining_words.
 
         reduced_words: the remaining words in `line`, after attempting to remove all words that
             exceed `max_chars` (rounding down to the nearest word boundary).
-
         remaining_words: the words in `line` which exceed `max_chars`. This value is None if
             no words could be split from `line`.
-
         If there are any remaining_words, an ellipses is appended to reduced_words and a
         continuation header is inserted before remaining_words to visually communicate the line
         continuation.
-
         Return a tuple in the format (reduced_words, remaining_words).
         """
         reduced_words = []
@@ -173,67 +185,46 @@ class LinePaginator(Paginator):
     @classmethod
     async def paginate(
         cls,
-        lines: List[str],
+        lines: t.List[str],
         ctx: Context,
-        embed: Embed,
+        embed: discord.Embed,
         prefix: str = "",
         suffix: str = "",
-        max_lines: Optional[int] = None,
+        max_lines: t.Optional[int] = None,
         max_size: int = 500,
-        scale_to_size: int = 2000,
+        scale_to_size: int = 4000,
         empty: bool = True,
         restrict_to_user: User = None,
         timeout: int = 300,
         footer_text: str = None,
         url: str = None,
-        exception_on_empty_embed: bool = False
-    ) -> Optional[Message]:
+        exception_on_empty_embed: bool = False,
+    ) -> t.Optional[discord.Message]:
         """
         Use a paginator and set of reactions to provide pagination over a set of lines.
 
         The reactions are used to switch page, or to finish with pagination.
-
         When used, this will send a message using `ctx.send()` and apply a set of reactions to it. These reactions may
         be used to change page, or to remove pagination from the message.
-
         Pagination will also be removed automatically if no reaction is added for five minutes (300 seconds).
-
+        The interaction will be limited to `restrict_to_user` (ctx.author by default) or
+        to any user with a moderation role.
         Example:
-        >>> embed = Embed()
+        >>> embed = discord.Embed()
         >>> embed.set_author(name="Some Operation", url=url, icon_url=icon)
         >>> await LinePaginator.paginate([line for line in lines], ctx, embed)
         """
-        def event_check(reaction_: Reaction, user_: Member) -> bool:
-            """Make sure that this reaction is what we want to operate on."""
-            no_restrictions = (
-                # Pagination is not restricted
-                not restrict_to_user
-                # The reaction was by a whitelisted user
-                or user_.id == restrict_to_user.id
-            )
-
-            return (
-                # Conditions for a successful pagination:
-                all((
-                    # Reaction is on this message
-                    reaction_.message.id == message.id,
-                    # Reaction is one of the pagination emotes
-                    str(reaction_.emoji) in PAGINATION_EMOJI,
-                    # Reaction was not made by the Bot
-                    user_.id != ctx.bot.user.id,
-                    # There were no restrictions
-                    no_restrictions
-                ))
-            )
-
         paginator = cls(prefix=prefix, suffix=suffix, max_size=max_size, max_lines=max_lines,
                         scale_to_size=scale_to_size)
         current_page = 0
 
+        if not restrict_to_user:
+            restrict_to_user = ctx.author
+
         if not lines:
             if exception_on_empty_embed:
                 log.exception("Pagination asked for empty lines iterable")
-                raise EmptyPaginatorEmbed("No lines to paginate")
+                raise EmptyPaginatorEmbedError("No lines to paginate")
 
             log.debug("No lines to add to paginator, adding '(nothing to display)' message")
             lines.append("(nothing to display)")
@@ -283,9 +274,16 @@ class LinePaginator(Paginator):
             log.trace(f"Adding reaction: {repr(emoji)}")
             await message.add_reaction(emoji)
 
+        check = partial(
+            messages.reaction_check,
+            message_id=message.id,
+            allowed_emoji=PAGINATION_EMOJI,
+            allowed_users=(restrict_to_user.id,),
+        )
+
         while True:
             try:
-                reaction, user = await ctx.bot.wait_for("reaction_add", timeout=timeout, check=event_check)
+                reaction, user = await ctx.bot.wait_for("reaction_add", timeout=timeout, check=check)
                 log.trace(f"Got reaction: {reaction}")
             except asyncio.TimeoutError:
                 log.debug("Timed out waiting for a reaction")
@@ -360,7 +358,7 @@ class LinePaginator(Paginator):
                 await message.edit(embed=embed)
 
         log.debug("Ending pagination and clearing reactions.")
-        with suppress(NotFound):
+        with suppress(discord.NotFound):
             await message.clear_reactions()
 
 
@@ -397,7 +395,7 @@ class ImagePaginator(Paginator):
         self.images.append(image)
 
     @classmethod
-    async def paginate(cls, pages: List[Tuple[str, str]], ctx: Context, embed: Embed,
+    async def paginate(cls, pages: t.List[t.Tuple[str, str]], ctx: Context, embed: discord.Embed,
                        prefix: str = "", suffix: str = "", timeout: int = 300,
                        exception_on_empty_embed: bool = False):
         """
@@ -416,7 +414,7 @@ class ImagePaginator(Paginator):
         >>> embed.set_author(name="Some Operation", url=url, icon_url=icon)
         >>> await ImagePaginator.paginate(pages, ctx, embed)
         """
-        def check_event(reaction_: Reaction, member: Member) -> bool:
+        def check_event(reaction_: discord.Reaction, member: discord.Member) -> bool:
             """Checks each reaction added, if it matches our conditions pass the wait_for."""
             return all((
                 # Reaction is on the same message sent
@@ -433,7 +431,7 @@ class ImagePaginator(Paginator):
         if not pages:
             if exception_on_empty_embed:
                 log.exception("Pagination asked for empty image list")
-                raise EmptyPaginatorEmbed("No images to paginate")
+                raise EmptyPaginatorEmbedError("No images to paginate")
 
             log.debug("No images to add to paginator, adding '(no images to display)' message")
             pages.append(("(no images to display)", ""))
