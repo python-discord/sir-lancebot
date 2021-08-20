4f5432a97bfafab237efa6efa7bb6daebc83577d
D0rs4n <41237606+D0rs4n@users.noreply.github.com>
2021-08-20 21:25:17 +0200

    Adjust spacing in the Wikipedia Cog
    Co-authored-by: Bluenix <bluenixdev@gmail.com>

--- bot/utils/exceptions.py
+++ bot/utils/exceptions.py
@@ -8,7 +8,7 @@ class UserNotPlayingError(Exception):
 
 
 class APIError(Exception):
-    """Raised when an external API(eg. Wikipedia) returns an error response."""
+    """Raised when an external API (eg. Wikipedia) returns an error response."""
 
     def __init__(self, api: str, status_code: int, error_msg: Optional[str] = None):
         super().__init__()
