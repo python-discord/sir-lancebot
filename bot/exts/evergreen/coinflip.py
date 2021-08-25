6a92d64d9f113a02178cf05fd34c4169446c1842
Izan <tizzysaurus@gmail.com>
2021-08-25 21:37:57 +0100

    General improvements
    Fix typo in docstring, change logic for creating message to be smarter, change CoinSide converter logic to be smarter

--- bot/exts/evergreen/coinflip.py
+++ bot/exts/evergreen/coinflip.py
@@ -18,11 +18,10 @@ class CoinSide(commands.Converter):
         if side in CoinSide.HEADS:
             return "heads"
 
-        elif side in CoinSide.TAILS:
+        if side in CoinSide.TAILS:
             return "tails"
 
-        else:
-            raise commands.BadArgument(f"{side!r} is not a valid coin side.")
+        raise commands.BadArgument(f"{side!r} is not a valid coin side.")
 
 
 class CoinFlip(commands.Cog):
@@ -33,16 +32,14 @@ class CoinFlip(commands.Cog):
         """
         Flips a coin.
 
-        If `coin_side` is provided will state whether you guessed the side correctly.
+        If `side` is provided will state whether you guessed the side correctly.
         """
         flipped_side = random.choice(["heads", "tails"])
 
-        if not side:
-            await ctx.send(f"{ctx.author.mention} flipped **{flipped_side}**!")
-            return
-
         message = f"{ctx.author.mention} flipped **{flipped_side}**. "
-        if side == flipped_side:
+        if not side:
+            await ctx.send(message)
+        elif side == flipped_side:
             message += f"You guessed correctly! {Emojis.lemon_hyperpleased}"
         else:
             message += f"You guessed incorrectly. {Emojis.lemon_pensive}"
