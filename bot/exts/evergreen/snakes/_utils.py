9d82d7d9eecbddec4db80869e2c7343114779f57
KittyBorgX <krishnaramasimha12@gmail.com>
2021-08-29 07:41:54 +0530

    Lint Files and remove whitespace through poetry run task lint

--- bot/exts/evergreen/snakes/_utils.py
+++ bot/exts/evergreen/snakes/_utils.py
@@ -55,7 +55,7 @@ snakes = {
     "Baby Rattle Snake": "https://i.imgur.com/i5jYA8f.png",
     "Baby Dragon Snake": "https://i.imgur.com/SuMKM4m.png",
     "Baby Garden Snake": "https://i.imgur.com/5vYx3ah.png",
-    "Baby Cobra": "https://i.imgur.com/jk14ryt.png", 
+    "Baby Cobra": "https://i.imgur.com/jk14ryt.png",
     "Baby Anaconda": "https://i.imgur.com/4zT4aij.png",
 }
 
