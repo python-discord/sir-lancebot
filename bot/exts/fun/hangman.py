5e6dd1801ff7f771a01fbb98afbe61ef66a21619
Shom770 <82843611+Shom770@users.noreply.github.com>
2021-09-06 13:12:56 -0400

    fixing style inconsistencies and other problems with how the code looks - as per requested by Objectivix

--- bot/exts/fun/hangman.py
+++ bot/exts/fun/hangman.py
@@ -7,6 +7,16 @@ from discord.ext import commands
 
 from bot.bot import Bot
 
+TOP_WORDS_FILE_PATH = Path("bot/resources/fun/top_1000_used_words.txt").resolve()
+# defining a list of images that will be used for the game to represent the hangman person
+IMAGES = ["https://cdn.discordapp.com/attachments/859123972884922418/883472355056295946/hangman0.png",
+          "https://cdn.discordapp.com/attachments/859123972884922418/883472756744814613/hangman1.png",
+          "https://cdn.discordapp.com/attachments/859123972884922418/883472808699629578/hangman2.png",
+          "https://cdn.discordapp.com/attachments/859123972884922418/883472862441267230/hangman3.png",
+          "https://cdn.discordapp.com/attachments/859123972884922418/883472950991396864/hangman4.png",
+          "https://cdn.discordapp.com/attachments/859123972884922418/883472999431430204/hangman5.png",
+          "https://cdn.discordapp.com/attachments/859123972884922418/883473051277226015/hangman6.png"]
+
 
 class Hangman(commands.Cog):
     """
@@ -17,60 +27,69 @@ class Hangman(commands.Cog):
 
     def __init__(self, bot: Bot):
         self.bot = bot
-        with Path("bot/resources/fun/top_1000_used_words.txt").resolve().open(mode="r", encoding="utf-8") as file:
-            self.all_words = list(map(lambda line: line.strip("\n"), file.readlines()))
+        with TOP_WORDS_FILE_PATH.open(mode="r", encoding="utf-8") as file:
+            self.all_words = [line.strip('\n') for line in file.readlines()]
 
     @commands.command()
-    async def hangman(self, ctx: commands.Context, min_length: str = "0", max_length: str = "25",
-                      min_unique_letters: str = "0", max_unique_letters: str = "25") -> None:
+    async def hangman(
+            self,
+            ctx: commands.Context,
+            min_length: str = "0",
+            max_length: str = "25",
+            min_unique_letters: str = "0",
+            max_unique_letters: str = "25"
+    ) -> None:
         """
         Play against the bot, where you have to guess the word it has provided!
 
         You have 7 tries, and you have to guess the letters that are in the word, or you lose!
         """
         # filtering the list of all words depending on the configuration
-        filtered_words = list(filter(lambda x: int(min_length) < len(x) < int(max_length)
-                                     and int(min_unique_letters) < len(set(x)) < int(max_unique_letters),
-                                     self.all_words))
+        filtered_words = [word for word in self.all_words
+                          if int(min_length) < len(word) < int(max_length)
+                          and int(min_unique_letters) < len(set(word)) < int(max_unique_letters)]
+
         if not filtered_words:
-            filter_not_found_embed = Embed(title="With the filters you provided, I could not find any words "
-                                                 "that fit all the filters specified..", color=Color.red())
+            filter_not_found_embed = Embed(
+                title="With the filters you provided, I could not find any words that fit all the filters specified..",
+                color=Color.red()
+            )
             await ctx.send(embed=filter_not_found_embed)
             return
 
-        # defining a list of images that will be used for the game to represent the hangman person
-        images = ["https://cdn.discordapp.com/attachments/859123972884922418/883472355056295946/hangman0.png",
-                  "https://cdn.discordapp.com/attachments/859123972884922418/883472756744814613/hangman1.png",
-                  "https://cdn.discordapp.com/attachments/859123972884922418/883472808699629578/hangman2.png",
-                  "https://cdn.discordapp.com/attachments/859123972884922418/883472862441267230/hangman3.png",
-                  "https://cdn.discordapp.com/attachments/859123972884922418/883472950991396864/hangman4.png",
-                  "https://cdn.discordapp.com/attachments/859123972884922418/883472999431430204/hangman5.png",
-                  "https://cdn.discordapp.com/attachments/859123972884922418/883473051277226015/hangman6.png"]
-
         # a dictionary mapping the images of the "hung man" to the number of tries it corresponds to
-        mapping_of_images = {tries_key: image_name for image_name, tries_key in zip(images, range(6, -1, -1))}
+        mapping_of_images = {tries_key: image_name for tries_key, image_name in zip(range(6, -1, -1), IMAGES)}
 
         word = choice(filtered_words)
+
         user_guess = "_" * len(word)
         tries = 6
+
         hangman_embed = Embed(title="Hangman", color=Color.blurple())
         hangman_embed.set_image(url=mapping_of_images[tries])
-        hangman_embed.add_field(name=f"The word is `{user_guess}`", value="Guess the word by sending a "
-                                                                          "message with the letter!", inline=False)
+        hangman_embed.add_field(
+            name=f"The word is `{user_guess}`",
+            value="Guess the word by sending a message with the letter!",
+            inline=False
+        )
         original_message = await ctx.send(embed=hangman_embed)
 
         while user_guess != word:
             try:
-                message = await self.bot.wait_for(event="message", timeout=30.0,
-                                                  check=lambda msg: msg.author != self.bot)
+                message = await self.bot.wait_for(
+                    event="message",
+                    timeout=30.0,
+                    check=lambda msg: msg.author != self.bot
+                )
 
                 if len(message.content) > 1:
                     continue
 
                 elif message.content in word:
                     positions = [idx for idx, letter in enumerate(word) if letter == message.content]
-                    user_guess = "".join([message.content if index in positions else dash
-                                          for index, dash in enumerate(user_guess)])
+                    user_guess = "".join(
+                        [message.content if index in positions else dash for index, dash in enumerate(user_guess)]
+                    )
 
                 elif tries - 1 <= 0:
                     losing_embed = Embed(title="You lost.", description=f"The word was `{word}`.", color=Color.red())
@@ -82,9 +101,11 @@ class Hangman(commands.Cog):
 
                 hangman_embed.clear_fields()
                 hangman_embed.set_image(url=mapping_of_images[tries])
-                hangman_embed.add_field(name=f"The word is `{user_guess}`", value="Guess the word by sending a "
-                                                                                  "message with the letter!",
-                                        inline=False)
+                hangman_embed.add_field(
+                    name=f"The word is `{user_guess}`",
+                    value="Guess the word by sending a message with the letter!",
+                    inline=False
+                )
                 await original_message.edit(embed=hangman_embed)
             except TimeoutError:
                 return
@@ -94,5 +115,5 @@ class Hangman(commands.Cog):
 
 
 def setup(bot: Bot) -> None:
-    """Setting up the Hangman cog."""
+    """Load the Hangman cog."""
     bot.add_cog(Hangman(bot))
