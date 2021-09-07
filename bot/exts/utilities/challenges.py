f83d024a0db0ae56defd0016feaeb1a926292632
Shom770 <82843611+Shom770@users.noreply.github.com>
2021-09-07 18:38:02 -0400

    replacing ' with a double quote for some strings

--- bot/exts/utilities/challenges.py
+++ bot/exts/utilities/challenges.py
@@ -9,7 +9,7 @@ from bot.bot import Bot
 from bot.constants import Colours, Emojis, NEGATIVE_REPLIES
 
 logger = logging.getLogger(__name__)
-API_ROOT = 'https://www.codewars.com/api/v1/code-challenges/{kata_id}'
+API_ROOT = "https://www.codewars.com/api/v1/code-challenges/{kata_id}"
 
 
 class Challenges(commands.Cog):
@@ -23,7 +23,7 @@ class Challenges(commands.Cog):
         self.bot = bot
 
     @commands.command(aliases=["kata"])
-    async def challenge(self, ctx: commands.Context, language: str = 'python', *, query: str = None) -> None:
+    async def challenge(self, ctx: commands.Context, language: str = "python", *, query: str = None) -> None:
         """
         The challenge command pulls a random kata (challenge) from codewars.com.
 
@@ -35,12 +35,12 @@ class Challenges(commands.Cog):
         `.challenge <language> <query>, <difficulty>` - Pulls a random challenge with the query provided,
         under that difficulty within the language's scope.
         """
-        get_kata_link = f'https://codewars.com/kata/search/{language}'
+        get_kata_link = f"https://codewars.com/kata/search/{language}"
 
         if language and not query:
             level = f"-{choice(['1', '2', '3', '4', '5', '6', '7', '8'])}"
-        elif ',' in query or ', ' in query:
-            query_splitted = query.split(',' if ', ' not in query else ', ')
+        elif "," in query or ", " in query:
+            query_splitted = query.split("," if ", " not in query else ", ")
 
             if len(query_splitted) > 2:
                 raise commands.BadArgument(
@@ -48,21 +48,21 @@ class Challenges(commands.Cog):
                 )
 
             query, level = query_splitted
-            level = f'-{level}'
+            level = f"-{level}"
         elif query.isnumeric():
-            level, query = f'-{query}', None
+            level, query = f"-{query}", None
         else:
             level = None
 
-        params = {name: value for name, value in zip(['q', 'r[]'], [query, level]) if value}
-        params = {**params, 'beta': 'false'}
+        params = {name: value for name, value in zip(["q", "r[]"], [query, level]) if value}
+        params = {**params, "beta": "false"}
         async with self.bot.http_session.get(get_kata_link, params=params) as response:
             if response.status != 200:
                 logger.error(
                     f"Unexpected status code {response.status} from codewars.com"
                 )
-            soup = BeautifulSoup(await response.text(), features='lxml')
-            first_kata_div = soup.find_all('div', class_='item-title px-0')
+            soup = BeautifulSoup(await response.text(), features="lxml")
+            first_kata_div = soup.find_all("div", class_="item-title px-0")
 
             if not first_kata_div:
                 no_results_embed = Embed(
@@ -80,7 +80,7 @@ class Challenges(commands.Cog):
                 first_kata_div = first_kata_div[0]
 
             # there are numerous divs before arriving at the id of the kata, which can be used for the link.
-            first_kata_id = first_kata_div.a['href'].split('/')[-1]
+            first_kata_id = first_kata_div.a["href"].split("/")[-1]
 
         async with self.bot.http_session.get(API_ROOT.format(kata_id=first_kata_id)) as kata_information:
             if response.status != 200:
@@ -94,7 +94,7 @@ class Challenges(commands.Cog):
                 -4: (60, 126, 187), -3: (60, 126, 187), -2: (134, 108, 199), -1: (134, 108, 199)
             }
 
-            kata_description = kata_information['description']
+            kata_description = kata_information["description"]
 
             # ensuring it isn't over the length 1024
             if len(kata_description) > 1024:
