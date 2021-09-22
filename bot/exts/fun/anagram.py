a91a850401df72430c7cbbe6c7b2de34cab8a22c
maxyodedara5 <maxyodedara5@gmail.com>
2021-09-22 21:03:39 +0530

    Polish anagram command for multiple channels
    * Updated docstrings
    * Allowed command to be used in multiple channels
    * Create a class for anagram game instances
    * Lay groundwork for threads
    
    Co-Authored-By: Bluenix <bluenixdev@gmail.com>

--- bot/exts/fun/anagram.py
+++ bot/exts/fun/anagram.py
@@ -8,15 +8,36 @@ import discord
 from discord.ext import commands
 
 from bot.bot import Bot
-from bot.constants import Colours, NEGATIVE_REPLIES
+from bot.constants import Colours
 
 log = logging.getLogger(__name__)
 
 TIME_LIMIT = 60
 
 # anagram.json file contains all the anagrams
-with open(Path("bot/resources/fun/anagram.json")) as f:
-    ANAGRAMS_ALL = json.loads(f.read_text("utf8"))
+with open(Path("bot/resources/fun/anagram.json"), "r") as f:
+    ANAGRAMS_ALL = json.load(f)
+
+
+class AnagramGame:
+    """
+    Used for creating instances of anagram games.
+
+    Once multiple games can be run at the same time, this class' instances
+    can be used for keeping track of each anagram game.
+    """
+
+    def __init__(self, scrambled: str, correct: list[str]) -> None:
+        self.scrambled = scrambled
+        self.correct = set(correct)
+
+        self.winners = set()
+
+    async def message_creation(self, message: discord.Message) -> None:
+        """Check if the message is a correct answer and remove it from the list of answers."""
+        if message.content.lower() in self.correct:
+            self.winners.add(message.author.mention)
+            self.correct.remove(message.content.lower())
 
 
 class Anagram(commands.Cog):
@@ -24,43 +45,30 @@ class Anagram(commands.Cog):
 
     def __init__(self, bot: Bot):
         self.bot = bot
-        self.winners = set()
-        self.correct = ""
-        self.current_channel = None
+
+        self.games: dict[int, AnagramGame] = {}
 
     @commands.command(name="anagram", aliases=("anag", "gram", "ag"))
+    @commands.guild_only()
     async def anagram_command(self, ctx: commands.Context) -> None:
         """
-        Given shuffled letters which can be rearranged to form Anagrams.
+        Given shuffled letters rearrange them into anagrams.
 
-        It has code for anagram game command. Users can play the game by using ".anagram" command,
-        it will show an embed with scrambled letters which if rearranged can form words.
-        It selects random choices from a json file which has all the anagrams possible from popular English words.
-        After 60 seconds if anyone has provided a correct answer in channel,
-        it will list out the winner's name and if nobody gets its correct
-        then all the correct answers will be displayed after a minute.
+        Show an embed with scrambled letters which if rearranged can form words.
+        After specific amount of time list the correct answers and whether someone provided a
+        correct answer.
         """
-        if self.current_channel:
-            await ctx.send(f"An anagram is already being solved in {self.current_channel.mention}!")
+        if self.games.get(ctx.channel.id):
+            await ctx.send("An anagram is already being solved in this channel!")
             return
 
-        # Don't let users start in a DM
-        if not ctx.guild:
-            await ctx.send(
-                embed=discord.Embed(
-                    title=random.choice(NEGATIVE_REPLIES),
-                    description="You can't start anagram command in DMs",
-                    colour=discord.Colour.red(),
-                )
-            )
-            return
+        scrambled_letters, correct = random.choice(list(ANAGRAMS_ALL.items()))
 
-        self.current_channel = ctx.channel
-
-        scrambled_letters, self.correct = random.choice(list(ANAGRAMS_ALL.items()))
+        game = AnagramGame(scrambled_letters, correct)
+        self.games[ctx.channel.id] = game
 
         anagram_embed = discord.Embed(
-            title=f"Find anagrams from these letters '{scrambled_letters.upper()}'",
+            title=f"Find anagrams from these letters: '{scrambled_letters.upper()}'",
             description=f"You have {TIME_LIMIT} seconds to find correct words.",
             colour=Colours.purple,
         )
@@ -68,35 +76,35 @@ class Anagram(commands.Cog):
         await ctx.send(embed=anagram_embed)
         await asyncio.sleep(TIME_LIMIT)
 
-        if self.winners:
-            win_list = ", ".join(self.winners)
+        if game.winners:
+            win_list = ", ".join(game.winners)
             content = f"Well done {win_list} for getting it right!"
         else:
             content = "Nobody got it right."
 
         answer_embed = discord.Embed(
-            title=f"The words were:  `{'`, `'.join(self.correct)}`!",
+            title=f"The words were:  `{'`, `'.join(ANAGRAMS_ALL[game.scrambled])}`!",
             colour=Colours.pink,
         )
 
         await ctx.send(content, embed=answer_embed)
 
-        self.winners.clear()
-        self.current_channel = None
+        # Game is finished, let's remove it from the dict
+        self.games.pop(ctx.channel.id)
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message) -> None:
-        """If a non-bot user enters a correct answer, their username gets added to self.winners."""
-        if self.current_channel != message.channel:
+        """Check a message for an anagram attempt and pass to an ongoing game."""
+        if message.author.bot or not message.guild:
             return
 
-        if self.bot.user == message.author:
+        game = self.games.get(message.channel.id)
+        if not game:
             return
 
-        if message.content.lower() in self.correct:
-            self.winners.add(message.author.mention)
+        await game.message_creation(message)
 
 
 def setup(bot: Bot) -> None:
-    """Loads the Anagram cog."""
+    """Load the Anagram cog."""
     bot.add_cog(Anagram(bot))
