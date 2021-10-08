71d800add94df3d678786ab482747ae904290129
Izan <tizzysaurus@gmail.com>
2021-10-08 15:04:35 +0100

    Rename `Roles.admin` to `Roles.admins`

--- bot/constants.py
+++ bot/constants.py
@@ -280,9 +280,9 @@ if Client.month_override is not None:
 
 
 class Roles(NamedTuple):
-    owner = 267627879762755584
-    admin = int(environ.get("BOT_ADMIN_ROLE_ID", 267628507062992896))
-    moderator = 267629731250176001
+    owners = 267627879762755584
+    admins = int(environ.get("BOT_ADMIN_ROLE_ID", 267628507062992896))
+    moderation_team = 267629731250176001
     helpers = int(environ.get("ROLE_HELPERS", 267630620367257601))
     core_developers = 587606783669829632
     everyone = int(environ.get("BOT_GUILD", 267624335836053506))
@@ -332,8 +332,8 @@ class Reddit:
 
 
 # Default role combinations
-MODERATION_ROLES = Roles.moderation_team, Roles.admin, Roles.owner
-STAFF_ROLES = Roles.helpers, Roles.moderation_team, Roles.admin, Roles.owner
+MODERATION_ROLES = Roles.moderation_team, Roles.admins, Roles.owner
+STAFF_ROLES = Roles.helpers, Roles.moderation_team, Roles.admins, Roles.owner
 
 # Whitelisted channels
 WHITELISTED_CHANNELS = (
