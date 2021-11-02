614179f80e27dfee30d0cd8fedb9718e5f07a89e
Nipa-Code <wtblitzo@gmail.com>
2021-11-02 13:07:48 +0200

    Limit user reactions on embed pagination
    Limit user reactions to prevent non-author from removing message by
    adding user restriction to paginator.

--- bot/exts/utilities/wikipedia.py
+++ bot/exts/utilities/wikipedia.py
@@ -87,7 +87,7 @@ class WikipediaSearch(commands.Cog):
             embed.set_thumbnail(url=WIKI_THUMBNAIL)
             embed.timestamp = datetime.utcnow()
             await LinePaginator.paginate(
-                contents, ctx, embed
+                contents, ctx, embed, restrict_to_user=ctx.author
             )
         else:
             await ctx.send(
