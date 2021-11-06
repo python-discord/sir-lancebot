8f218c9d2b4baad02ef49842b39dc7bd9de18fd1
DMFriends <miretsky.daniel44@gmail.com>
2021-11-05 22:09:06 -0400

    More changes to Madlibs
    (not functional yet)
    Added a new list where all the words entered by the user get saved
    Tried to join that list to the blanks in the original story but that didn't quite work out

--- bot/exts/fun/madlibs.py
+++ bot/exts/fun/madlibs.py
@@ -32,16 +32,14 @@ class Madlibs(commands.Cog):
 
     @staticmethod
     def _load_templates() -> list:
-        madlibs_stories = Path("bot/resources/fun/madlibs_templates.json")
+        madlibs_stories = Path("bot/resources/fun/madlibs_templates (1).json")
 
         with open(madlibs_stories) as file:
             return json.load(file)
 
     @staticmethod
-    def madlibs_embed(random_template: list, current_input: int, part_of_speech: str, number_of_inputs: int) -> Embed:
+    def madlibs_embed(part_of_speech: str, number_of_inputs: int) -> Embed:
         """Method to have the bot send an embed with the game information."""
-        part_of_speech = random_template["blanks"][current_input]
-
         madlibs_embed = Embed(
             title="Madlibs",
             color=Colours.python_blue,
@@ -49,8 +47,10 @@ class Madlibs(commands.Cog):
 
         madlibs_embed.add_field(
             name="Enter a word that fits the given part of speech!",
-            value=f"Please enter a(n) {part_of_speech}!"
+            value=f"Part of speech: {part_of_speech}"
         )
+
+        madlibs_embed.set_footer(text=f'Inputs remaining: {number_of_inputs}')
 
         return madlibs_embed
 
@@ -61,25 +61,21 @@ class Madlibs(commands.Cog):
     ) -> None:
         """Play Madlibs with the bot, where you have to enter a word that fits the part of speech that you are given!"""
         random_template = choice(self.templates["templates"])
+        # random_template = self.templates["templates"][0]
 
         current_input = 0
+
         number_of_inputs = len(random_template["blanks"])
 
         part_of_speech = random_template["blanks"][current_input]
 
-        madlibs_embed = Embed(
-            title="Madlibs",
-            color=Colours.python_blue,
-        )
-
-        madlibs_embed.add_field(
-            name="Enter a word that fits the given part of speech!",
-            value=f"Please enter a(n) {part_of_speech}!"
-        )
+        madlibs_embed = self.madlibs_embed(part_of_speech, number_of_inputs)
 
         original_message = await ctx.send(embed=madlibs_embed)
 
-        while current_input != 30:
+        while True:
+            part_of_speech = random_template["blanks"][current_input]
+
             try:
                 message = await self.bot.wait_for(event='message', timeout=TIMEOUT)
             except TimeoutError:
@@ -91,27 +87,37 @@ class Madlibs(commands.Cog):
                 return
 
             word = message.content
-            for blank in random_template["value"]:
-                blank = ''
-                blank.replace('', word)
+            submitted_words = []
+            submitted_words += word
+
+            # str_template = ' '.join(random_template["value"])
+            # for word, blank in zip(submitted_words, random_template["value"]):
+            #     word_in_story = blank.replace("", word)
+
+            # random_template["value"] += submitted_words
 
             current_input += 1
             number_of_inputs -= 1
 
             madlibs_embed.clear_fields()
 
-            madlibs_embed = self.new_embed_fields(random_template, current_input, part_of_speech, number_of_inputs)
+            madlibs_embed = self.madlibs_embed(part_of_speech, number_of_inputs)
 
             await original_message.edit(embed=madlibs_embed)
 
+            if number_of_inputs == 0:
+                break
+
+        str_template = ' '.join(random_template["value"])
+        str_template_with_words = str_template.join(submitted_words)
+
         story_embed = Embed(
             title=random_template["title"],
-            description=str(random_template["value"]),
+            description=str_template_with_words,
             color=Colours.bright_green,
         )
 
         await ctx.send(embed=story_embed)
-        return
 
 
 def setup(bot: Bot) -> None:
