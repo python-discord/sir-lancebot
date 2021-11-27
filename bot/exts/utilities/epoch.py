ab41c0aa55e02cf6660add7d7e6fcfdd3840d12f
Sn4u <Sn4u@github.com>
2021-11-27 23:45:13 +0000

    Added converter

--- bot/exts/utilities/epoch.py
+++ bot/exts/utilities/epoch.py
@@ -1,11 +1,27 @@
 from discord.ext import commands
+from discord.ext.commands import Converter
 from bot.bot import Bot
+from typing import Union
+
+import dateutil
+from dateutil import parser
+import arrow
+
+
+class RelativeDate(Converter):
+    async def convert(self, ctx: commands.Context, argument: str) -> arrow.Arrow:
+        return arrow.utcnow().dehumanize(argument)
+
+
+class AbsoluteDate(Converter):
+    async def convert(self, ctx: commands.Context, argument: str) -> arrow.Arrow:
+        return arrow.get(dateutil.parser.parse(argument))
 
 
 class Epoch(commands.Cog):
 
     @commands.command(name="epoch")
-    async def epoch(self, ctx: commands.Context, *, time) -> None:
+    async def epoch(self, ctx: commands.Context, *, time: Union[RelativeDate, AbsoluteDate]) -> None:
         pass
 
 
