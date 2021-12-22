06adc2ba91663a5221ef4084b29b19c6d804fef4
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2021-12-22 22:49:35 +0100

    Add logic for extracting codepoint from user input
    Fix bug where U+XXXX included "+" in codepoint
    
    Also makes sure that the parsed codepoint doesn't have leading zeroes,
    since twemoji files don't include them

--- bot/exts/utilities/twemoji.py
+++ bot/exts/utilities/twemoji.py
@@ -1,17 +1,21 @@
 import logging
-from typing import Literal
+import re
+from typing import List, Literal
 
 import discord
 from discord.ext import commands
+from emoji import is_emoji
 
 from bot.bot import Bot
 from bot.constants import Colours
+from bot.utils.extensions import invoke_help_command
 
 log = logging.getLogger(__name__)
 BASE_URLS = {
     "png": "https://raw.githubusercontent.com/twitter/twemoji/master/assets/72x72/",
     "svg": "https://raw.githubusercontent.com/twitter/twemoji/master/assets/svg/",
 }
+CODE = re.compile(r"[a-f1-9][a-f0-9]{3,5}$")
 
 
 class Twemoji(commands.Cog):
@@ -32,13 +36,51 @@ class Twemoji(commands.Cog):
             colour=Colours.twitter_blue,
         )
         embed.set_thumbnail(url=Twemoji.get_url(codepoint, "png"))
-        embed.set_footer(text=codepoint)
+        embed.set_footer(text=codepoint.replace("-", " "))
 
         return embed
 
+    @staticmethod
+    def emoji(codepoint: str) -> str:
+        if code := Twemoji.trim_code(codepoint):
+            return chr(int(code, 16))
+        return ""
+
+    @staticmethod
+    def codepoint(emoji: str) -> str:
+        return hex(ord(emoji))[2:]
+
+    @staticmethod
+    def trim_code(codepoint: str) -> str:
+        if code := CODE.search(str(codepoint)):
+            return code.group()
+
+    def codepoint_from_input(self, raw_emoji: List[str]):
+        if is_emoji(raw_emoji[0]):
+            emojis = (self.codepoint(emoji) for emoji in raw_emoji[0])
+            return "-".join(emojis)
+
+        raw_first = self.trim_code(raw_emoji[0])
+        if is_emoji("".join(self.emoji(self.trim_code(code)) for code in raw_emoji)):
+            return "-".join(self.trim_code(code) for code in raw_emoji)
+        if is_emoji(self.emoji(raw_first)):
+            return raw_first
+
+        raise ValueError("No codepoint could be obtained from the given input")
+
     @commands.command(aliases=("tw",))
-    async def twemoji(self, ctx: commands.Context, emoji: str) -> None:
-        await ctx.send(embed=self.build_embed(emoji))
+    async def twemoji(self, ctx: commands.Context, *raw_emoji: str) -> None:
+        if len(raw_emoji) == 0:
+            await invoke_help_command(ctx)
+            return
+        try:
+            codepoint = self.codepoint_from_input(raw_emoji)
+        except (ValueError, AttributeError) as e:
+            raise commands.BadArgument(
+                "please include a valid emoji or emoji code point."
+            )
+
+        await ctx.send(embed=self.build_embed(codepoint))
 
 
 def setup(bot: Bot) -> None:
