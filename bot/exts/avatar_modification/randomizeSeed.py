aab5f5b40120fbf8ba2d8811b9c96fa613370267
AFK-debug-9 <75903679+AFK-debug-9@users.noreply.github.com>
2022-02-14 11:27:10 +0530

    Create randomizeSeed.py

---
+++ bot/exts/avatar_modification/randomizeSeed.py
@@ -0,0 +1,27 @@
+import random, threading
+
+def thread():
+  bin = ''
+  for i in range(5000*8):
+    bin += random.choice('10')
+    
+  random.seed(str(int(bin,2))+bin)
+
+def randomize():
+  threadlings = []
+
+  for i in range(10):
+    threadlings.append(threading.Thread(target=thread))
+
+  for threadling in threadlings:
+    threadling.start()
+
+  for threadling in threadlings:
+    threadling.join()
+
+  del threadlings # Sad :(
+  
+  for i in range(5000*8):
+    bin += random.choice('10')
+    
+  return str(int(bin,2))+bin)
