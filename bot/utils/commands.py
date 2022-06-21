44d908a880d39b9f7a873456f1684476e3491f32
RohanJnr <rohanjnr44@gmail.com>
2022-06-22 01:34:53 +0530

    add docstring

--- bot/utils/commands.py
+++ bot/utils/commands.py
@@ -4,5 +4,6 @@ from rapidfuzz import process
 
 
 def get_command_suggestions(all_commands: list[str], query: str, *, cutoff=60, limit=3) -> Optional[list]:
+    """Get similar command names."""
     results = process.extract(query, all_commands, score_cutoff=cutoff, limit=limit)
     return [result[0] for result in results]
