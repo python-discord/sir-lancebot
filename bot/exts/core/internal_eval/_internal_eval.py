fb658c14f67eaf4ccf900482e887cc5e945fc408
shtlrs <amrbellalouna@gmail.com>
2023-05-01 14:27:11 +0100

    update forgotten reference of `Bot`

--- bot/exts/core/internal_eval/_internal_eval.py
+++ bot/exts/core/internal_eval/_internal_eval.py
@@ -7,7 +7,7 @@ import discord
 from discord.ext import commands
 
 from bot.bot import Bot
-from bot.constants import Client, Roles
+from bot.constants import Bot, Roles
 from bot.utils.decorators import with_role
 
 from ._helpers import EvalContext
