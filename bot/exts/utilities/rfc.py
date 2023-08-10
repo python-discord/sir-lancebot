3a9be593adeb56b3d4aa4b34d178189e1e777b0f
SomeHybrid <91648368+SomeHybrid@users.noreply.github.com>
2023-08-10 13:53:46 +0700

    Applied `isort` to `rfc.py`

--- bot/exts/utilities/rfc.py
+++ bot/exts/utilities/rfc.py
@@ -1,8 +1,8 @@
-import logging
 import datetime
+import logging
 
-from discord.ext import commands
 from discord import Embed
+from discord.ext import commands
 
 from bot.bot import Bot
 from bot.constants import Colours
