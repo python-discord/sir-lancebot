44920212acf0116399c14dfc11f083285ca9c28e
shtlrs <amrbellalouna@gmail.com>
2024-02-18 21:31:51 +0100

    fixup: correct UserNotPlayingError

--- bot/command_error_handlers/user_not_playing.py
+++ bot/command_error_handlers/user_not_playing.py
@@ -1,8 +1,10 @@
 from typing import NoReturn
 
 from discord import Interaction
-from discord.ext.commands import CommandOnCooldown, Context
+from discord.ext.commands import Context
 from pydis_core.utils.error_handling.commands import AbstractCommandErrorHandler
+
+from bot.utils.exceptions import UserNotPlayingError
 
 
 class UserNotPlayingErrorHandler(AbstractCommandErrorHandler):
@@ -10,7 +12,7 @@ class UserNotPlayingErrorHandler(AbstractCommandErrorHandler):
 
     async def should_handle_error(self, error: Exception) -> bool:
         """A predicate that determines whether the error should be handled or not."""
-        return isinstance(error, CommandOnCooldown)
+        return isinstance(error, UserNotPlayingError)
 
     async def handle_text_command_error(self, context: Context, error: Exception) -> NoReturn:
         """Handle error raised in the context of text commands."""
