68e228103d0aa10e1e8434028ca22671e4f33f4d
daDevBoat <113507675+daDevBoat@users.noreply.github.com>
2026-02-23 15:55:52 +0100

    feat: restucture of the mathdoku classes and new creation methods (#25)
    close #21

--- bot/exts/fun/mathdoku.py
+++ bot/exts/fun/mathdoku.py
@@ -1,25 +1,49 @@
 
+
 class Cell:
     """Represents a single cell in the grid."""
 
     def __init__(self, column: int, row: int) -> None:
         self.column = column
         self.row = row
+        self.block = None
+        self.guess = 0
 
 class Block:
     """Represents a block in the puzzle, with its cells, operation and colour."""
 
-    def __init__(self, cells: list[Cell], label: str, label_cell: Cell, colour: tuple[int, int, int]) -> None:
-        self.cells = cells
-        self.label = label
+    def __init__(self, operation: str, number: int, label_cell: Cell, colour: tuple[int, int, int]) -> None:
+        self.cells = []
+        self.operation = operation
+        self.number = number
         self.label_cell = label_cell
         self.colour = colour
 
 class Grid:
     """Represents the full game board, with all blocks and player guesses."""
 
-    def __init__(self, size: int, blocks: list[Block]) -> None:
+    def __init__(self, size: int) -> None:
         self.size = size
-        self.blocks = blocks
-        self.guesses: dict[tuple[int, int], int] = {}
-        self.cell_block_map: dict[tuple[int, int], Block] = {}
+        self.blocks = []
+        self.cells = []     # 2D array for cells [row][col]
+
+        self.__create_cells__()
+
+    def __create_cells__(self) -> None: 
+        for row in range(self.size):
+            self.cells.append([])
+            for col in range(self.size):
+                self.cells[row].append(Cell(col, row))
+
+    def __str__(self) -> str:
+        print_str = "\n"
+        for row in range(self.size):
+            print_str += "["
+            for col in range(self.size - 1):
+                print_str += str(self.cells[row][col].guess) + " "
+            print_str += str(self.cells[row][col].guess) + "]\n"
+        return print_str
+
+
+
+        
--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,9 +1,12 @@
 """
 How to run pytest:
-1. Check python --version and see that it is 3.13.X
-2. run pip install pytest
-3. run pip install pytest-asyncio
-4. run this to actually run the tests: python -m pytest tests/
+1. add venv or uv or install all dependecies uising:
+pip install pydis-core[all]==11.8.0 arrow==1.3.0 
+beautifulsoup4==4.12.3 colorama==0.4.6 coloredlogs==15.0.1 
+emoji==2.14.0 emojis==0.7.0 lxml==6.0.0 pillow==12.1.1 pydantic==2.10.1 
+pydantic-settings==2.8.1 pyjokes==0.8.3 PyYAML==6.0.2 rapidfuzz==3.12.2 sentry-sdk==2.19.2
+
+2. run this to actually run the tests: python -m pytest -s
 """
 
 
